80e4909865e7b9346ecbe319fc9d41d8f091b3b9
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-09-20 18:49:42 -0300

    RED: When creating a user, the id cannot be empty

---
+++ tests/VirtualPark.BusinessLogic.Test/User/Entity/UserTest.cs
@@ -0,0 +1,15 @@
+namespace VirtualPark.BusinessLogic.Test.User.Entity;
+
+[TestClass]
+[TestCategory("Entity")]
+[TestCategory("User")]
+public class UserTest
+{
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void User_WhenCreated_ShouldHaveNonEmptyId()
+    {
+        var user = new User();
+        user.Id.Should().NotBe(Guid.Empty);
+    }
+}

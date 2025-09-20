e91aa1770192f21ab7cb517a4831d67030cba93f
gdianesi <gd329180@fi365.ort.edu.uy>
2025-09-20 17:14:06 -0300

    RED: Visitor.Id should not be empty when created

--- tests/VirtualPark.BusinessLogic.Test/Visitor/VisitorTest.cs
+++ tests/VirtualPark.BusinessLogic.Test/Visitor/VisitorTest.cs
@@ -5,4 +5,14 @@ namespace VirtualPark.BusinessLogic.Test.Visitor;
 [TestCategory("Visitor")]
 public class VisitorTest
 {
+    [TestMethod]
+    [TestCategory("Constructor")]
+    public void WhenVisitorIsCreated_IdShouldBeAssigned()
+    {
+        // Act
+        var visitor = new Visitor();
+
+        // Assert
+        visitor.Id.Should().NotBe(Guid.Empty);
+    }
 }

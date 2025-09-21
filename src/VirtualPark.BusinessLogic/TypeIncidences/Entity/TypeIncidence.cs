2d01c90db9ece02202c3834d15c05abd22a83d27
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-09-21 20:03:26 -0300

    GREEN: the Type must be settable

--- src/VirtualPark.BusinessLogic/TypeIncidences/Entity/TypeIncidence.cs
+++ src/VirtualPark.BusinessLogic/TypeIncidences/Entity/TypeIncidence.cs
@@ -3,7 +3,7 @@ namespace VirtualPark.BusinessLogic.TypeIncidences.Entity;
 public class TypeIncidence
 {
     public Guid Id { get; }
-    public string Type { get; init; } = null!;
+    public string Type { get; set; } = null!;
 
     public TypeIncidence()
     {

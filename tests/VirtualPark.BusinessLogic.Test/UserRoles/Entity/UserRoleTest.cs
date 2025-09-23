b318027c36ab5596c8dfd137c0e7ddd72df0ff7f
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-09-23 20:29:51 -0300

    RED: the userId of UserRoles is gettable

---
+++ tests/VirtualPark.BusinessLogic.Test/UserRoles/Entity/UserRoleTest.cs
@@ -0,0 +1,20 @@
+using VirtualPark.BusinessLogic.Users.Entity;
+
+namespace VirtualPark.BusinessLogic.Test.UserRoles.Entity;
+
+[TestClass]
+[TestCategory("Entity")]
+[TestCategory("User")]
+public class UserRoleTest
+{
+    #region  UserId
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void UserRole_UserIdshoulBeGettable()
+    {
+        User user = new User();
+        var userRole = new UserRole { UserId = user.Id };
+        userRole.UserId.Should().Be(user.Id);
+    }
+    #endregion
+}

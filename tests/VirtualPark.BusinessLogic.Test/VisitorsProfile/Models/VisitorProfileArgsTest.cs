2f56d9ffffe33f0ff83577fb0a9d5969c6d79903
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-09-23 21:48:17 -0300

    RED: date of birth of visitor profile args is gettable

---
+++ tests/VirtualPark.BusinessLogic.Test/VisitorsProfile/Models/VisitorProfileArgsTest.cs
@@ -0,0 +1,15 @@
+namespace VirtualPark.BusinessLogic.Test.VisitorsProfile.Models;
+
+[TestClass]
+[TestCategory("Models")]
+[TestCategory("VisitorProfileArgsTest")]
+public class VisitorProfileArgsTest
+{
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void DateOfBirth_shouldBeGettable()
+    {
+        var visitorProfileArgs = new VisitorProfileArgs { DateOfBirth = new DateOnly(2002, 07, 30) };
+        visitorProfileArgs.DateOfBirth.Should().Be(new DateOnly(2002, 07, 30));
+    }
+}

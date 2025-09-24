d64c17cb3fada4d29f33ee4f12fc8f418c6224bc
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-09-23 21:25:27 -0300

    GREEN: name of user args is gettable

---
+++ src/VirtualPark.BusinessLogic/Users/Models/UserArgs.cs
@@ -0,0 +1,6 @@
+namespace VirtualPark.BusinessLogic.Users.Models;
+
+public class UserArgs
+{
+    public string Name { get; init; } = null!;
+}
--- tests/VirtualPark.BusinessLogic.Test/Users/Models/UserArgsTest.cs
+++ tests/VirtualPark.BusinessLogic.Test/Users/Models/UserArgsTest.cs
@@ -1,5 +1,11 @@
+using FluentAssertions;
+using VirtualPark.BusinessLogic.Users.Models;
+
 namespace VirtualPark.BusinessLogic.Test.Users.Models;
 
+[TestClass]
+[TestCategory("Models")]
+[TestCategory("UserArgs")]
 public class UserArgsTest
 {
     [TestMethod]

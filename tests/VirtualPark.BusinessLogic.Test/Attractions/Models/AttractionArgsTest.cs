51985b5a8f3e4576666cf109d2e246e8a4bd4847
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-09-24 18:21:45 -0300

    RED: Name_Getter_ReturnsAssignedValue

--- tests/VirtualPark.BusinessLogic.Test/Attractions/Models/AttractionArgsTest.cs
+++ tests/VirtualPark.BusinessLogic.Test/Attractions/Models/AttractionArgsTest.cs
@@ -11,10 +11,22 @@ public class AttractionArgsTest
 {
     #region Type
     [TestMethod]
+    [TestCategory("Validation")]
     public void Type_Getter_ReturnsAssignedValue()
     {
         var attractionArgs = new AttractionArgs("RollerCoaster");
         attractionArgs.Type.Should().Be("RollerCoaster");
+    }
+    #endregion
+
+    #region Name
+
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void Name_Getter_ReturnsAssignedValue()
+    {
+        var attractionArgs = new AttractionArgs("RollerCoaster", "The Big Bang");
+        attractionArgs.Name.Should().Be("The Big Bang");
     }
     #endregion
 }

a9161fcc79c54aef4a53f145b2265d2cf5075a17
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-09-23 21:52:28 -0300

    REFACTOR: date of birth of visitor profile args is gettable

--- tests/VirtualPark.BusinessLogic.Test/VisitorsProfile/Models/VisitorProfileArgsTest.cs
+++ tests/VirtualPark.BusinessLogic.Test/VisitorsProfile/Models/VisitorProfileArgsTest.cs
@@ -8,11 +8,13 @@ namespace VirtualPark.BusinessLogic.Test.VisitorsProfile.Models;
 [TestCategory("VisitorProfileArgsTest")]
 public class VisitorProfileArgsTest
 {
+    #region DateOfBirth
     [TestMethod]
     [TestCategory("Validation")]
-    public void DateOfBirth_shouldBeGettable()
+    public void DateOfBirth_Getter_ReturnsAssignedValue()
     {
         var visitorProfileArgs = new VisitorProfileArgs { DateOfBirth = new DateOnly(2002, 07, 30) };
         visitorProfileArgs.DateOfBirth.Should().Be(new DateOnly(2002, 07, 30));
     }
+    #endregion
 }

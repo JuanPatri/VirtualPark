5ebe2961c36fabe512dd6bd5d7d952362d91bc00
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-09-24 20:02:55 -0300

    GREEN: CurrentVisitor_Getter_ReturnsAssignedValue

--- src/VirtualPark.BusinessLogic/Attractions/Models/AttractionArgs.cs
+++ src/VirtualPark.BusinessLogic/Attractions/Models/AttractionArgs.cs
@@ -2,11 +2,12 @@ using VirtualPark.BusinessLogic.Validations.Services;
 
 namespace VirtualPark.BusinessLogic.Attractions.Models;
 
-public sealed class AttractionArgs(string type, string name, string miniumAge, string capacity, string description)
+public sealed class AttractionArgs(string type, string name, string miniumAge, string capacity, string description, string currentVisitor)
 {
     public string Type { get; init; } = type;
     public string Name { get; init; } = name;
     public int MiniumAge { get; init; } = ValidationServices.ValidateAndParseInt(miniumAge);
     public int Capacity { get; init; } = ValidationServices.ValidateAndParseInt(capacity);
     public string Description { get; init; } = description;
+    public int CurrentVisitor { get; init; } = ValidationServices.ValidateAndParseInt(currentVisitor);
 }

d7d6ec5574c12cc6d12a6c3904c86927c8a848bf
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-09-23 21:11:16 -0300

    RED WhenActiveStrategyIsCreated_IdIsAssigned

---
+++ tests/VirtualPark.BusinessLogic.Test/Strategy/Entity/ActiveStrategyTest.cs
@@ -0,0 +1,19 @@
+using VirtualPark.BusinessLogic.Strategy.Entity;
+
+namespace VirtualPark.BusinessLogic.Test.Strategy.Entity;
+
+[TestClass]
+[TestCategory("ActiveStrategy")]
+[TestCategory("Entity")]
+public class ActiveStrategyTest
+{
+    #region Id
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void WhenActiveStrategyIsCreated_IdIsAssigned()
+    {
+        var activeStrategy = new ActiveStrategy();
+        activeStrategy.Id.Should().NotBe(Guid.Empty);
+    }
+    #endregion
+}

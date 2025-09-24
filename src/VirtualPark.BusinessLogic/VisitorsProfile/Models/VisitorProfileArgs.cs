f166c169d53a7f0828fc80f41d8c8df0f1d16d08
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-09-23 21:55:58 -0300

    GREEN: membership of visitor profile args is gettable

--- src/VirtualPark.BusinessLogic/VisitorsProfile/Models/VisitorProfileArgs.cs
+++ src/VirtualPark.BusinessLogic/VisitorsProfile/Models/VisitorProfileArgs.cs
@@ -1,6 +1,9 @@
+using VirtualPark.BusinessLogic.Visitors.Entity;
+
 namespace VirtualPark.BusinessLogic.VisitorsProfile.Models;
 
 public class VisitorProfileArgs
 {
     public DateOnly DateOfBirth { get; init; }
+    public Membership Membership { get; init; }
 }

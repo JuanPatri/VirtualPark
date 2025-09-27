657193e74d107e912a9028e1814dfe3040b9887d
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-09-27 19:01:47 -0300

    REFACTOR: Description_ShouldParseStringGuid_ToGuidProperty

--- src/VirtualPark.BusinessLogic/Incidences/Models/IncidenceArgs.cs
+++ src/VirtualPark.BusinessLogic/Incidences/Models/IncidenceArgs.cs
@@ -5,5 +5,5 @@ namespace VirtualPark.BusinessLogic.Incidences.Models;
 public sealed class IncidenceArgs(string typeIncidence, string description)
 {
     public Guid TypeIncidence { get; init; } = ValidationServices.ValidateAndParseGuid(typeIncidence);
-    public string Description { get; set; } = ValidationServices.ValidateNullOrEmpty(description);
+    public string Description { get; init; } = ValidationServices.ValidateNullOrEmpty(description);
 }

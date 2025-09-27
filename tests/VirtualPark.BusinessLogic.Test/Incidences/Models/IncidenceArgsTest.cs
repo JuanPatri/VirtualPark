bf52ab01c2bbf25648d7fd1edda24d167cf37f32
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-09-27 18:59:44 -0300

    RED: Description_ShouldParseStringGuid_ToGuidProperty

--- tests/VirtualPark.BusinessLogic.Test/Incidences/Models/IncidenceArgsTest.cs
+++ tests/VirtualPark.BusinessLogic.Test/Incidences/Models/IncidenceArgsTest.cs
@@ -9,9 +9,7 @@ namespace VirtualPark.BusinessLogic.Test.Incidences.Models;
 public class IncidenceArgsTest
 {
     #region TypeIncidence
-
     [TestMethod]
-    [TestCategory("Incidences")]
     public void TypeIncidence_ShouldParseStringGuid_ToGuidProperty()
     {
         Guid expectedId = Guid.Parse("c8a0b0ef-9a4d-46e0-b9d3-0dfd68b6a010");
@@ -20,6 +18,14 @@ public class IncidenceArgsTest
 
         incidenceArgs.TypeIncidence.Should().Be(expectedId);
     }
+    #endregion
+    #region Description
 
+    [TestMethod]
+    public void Description_ShouldParseStringGuid_ToGuidProperty()
+    {
+        var incidenceArgs = new IncidenceArgs("c8a0b0ef-9a4d-46e0-b9d3-0dfd68b6a010", "Description");
+        incidenceArgs.Description.Sholud().Be("Description");
+    }
     #endregion
 }

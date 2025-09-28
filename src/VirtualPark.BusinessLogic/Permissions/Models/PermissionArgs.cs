d94476c551fedc36abdabf25384c1ba33a2bab1c
gdianesi <gd329180@fi365.ort.edu.uy>
2025-09-28 19:36:49 -0300

    REFACTOR: when descr. is null or empty should throw arg. excp

--- src/VirtualPark.BusinessLogic/Permissions/Models/PermissionArgs.cs
+++ src/VirtualPark.BusinessLogic/Permissions/Models/PermissionArgs.cs
@@ -1,18 +1,10 @@
+using VirtualPark.BusinessLogic.Validations.Services;
+
 namespace VirtualPark.BusinessLogic.Permissions.Models;
 
 public sealed class PermissionArgs(string description, string key, List<Guid> roles)
 {
-    public string Description { get; set; } = ValidateNullOrEmpty(description);
+    public string Description { get; set; } = ValidationServices.ValidateNullOrEmpty(description);
     public string Key { get; set; } = key;
     public List<Guid> Roles { get; set; } = roles;
-
-    private static string ValidateNullOrEmpty(string value)
-    {
-        if(string.IsNullOrWhiteSpace(value))
-        {
-            throw new ArgumentException("Value cannot be null or empty.");
-        }
-
-        return value;
-    }
 }

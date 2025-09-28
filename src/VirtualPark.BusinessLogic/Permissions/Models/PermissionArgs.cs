f1b2c776e8a52b5f63c827c08c81a3d3972de359
gdianesi <gd329180@fi365.ort.edu.uy>
2025-09-28 19:35:49 -0300

    GREEN: when descr. is null or empty should throw arg. excp

--- src/VirtualPark.BusinessLogic/Permissions/Models/PermissionArgs.cs
+++ src/VirtualPark.BusinessLogic/Permissions/Models/PermissionArgs.cs
@@ -2,7 +2,17 @@ namespace VirtualPark.BusinessLogic.Permissions.Models;
 
 public sealed class PermissionArgs(string description, string key, List<Guid> roles)
 {
-    public string Description { get; set; } = description;
+    public string Description { get; set; } = ValidateNullOrEmpty(description);
     public string Key { get; set; } = key;
     public List<Guid> Roles { get; set; } = roles;
+
+    private static string ValidateNullOrEmpty(string value)
+    {
+        if(string.IsNullOrWhiteSpace(value))
+        {
+            throw new ArgumentException("Value cannot be null or empty.");
+        }
+
+        return value;
+    }
 }

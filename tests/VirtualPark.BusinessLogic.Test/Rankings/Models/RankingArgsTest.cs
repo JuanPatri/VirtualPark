383dfb6e4d8228f74a53c900f8ec56ee2b02a1e2
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-09-29 20:13:32 -0300

    REFACOTR: list entries

--- tests/VirtualPark.BusinessLogic.Test/Rankings/Models/RankingArgsTest.cs
+++ tests/VirtualPark.BusinessLogic.Test/Rankings/Models/RankingArgsTest.cs
@@ -20,32 +20,33 @@ public class RankingArgsTest
     #region Users
 
     [TestMethod]
-    public void Entries_Getter_ReturnsValueAfterSet()
+    public void Entries_InitSetter_SetsValue_OnInitialization()
     {
-        var rankingArgs = new RankingArgs("2025-09-27 00:00", Array.Empty<string>());
         var expected = new List<Guid> { Guid.NewGuid(), Guid.NewGuid() };
 
-        rankingArgs.Entries = expected;
+        var rankingArgs = new RankingArgs("2025-09-27 00:00", Array.Empty<string>())
+        {
+            Entries = expected
+        };
 
         rankingArgs.Entries.Should().BeSameAs(expected);
         rankingArgs.Entries.Should().HaveCount(2);
     }
 
     [TestMethod]
-    public void Entries_Setter_ReplacesPreviousList()
+    public void Entries_InitSetter_OverridesCtorDefault()
     {
-        var rankingArgs = new RankingArgs("2025-09-27 00:00", Array.Empty<string>());
-        var first = new List<Guid> { Guid.NewGuid() };
-        var second = new List<Guid> { Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid() };
+        var ctorEntries = new[] { Guid.NewGuid().ToString() }; // lo que asignaría por defecto
+        var expected = new List<Guid> { Guid.NewGuid() };
 
-        rankingArgs.Entries = first;
-        rankingArgs.Entries = second;
+        var rankingArgs = new RankingArgs("2025-09-27 00:00", ctorEntries)
+        {
+            Entries = expected
+        };
 
-        rankingArgs.Entries.Should().BeSameAs(second);
-        rankingArgs.Entries.Should().NotBeSameAs(first);
-        rankingArgs.Entries.Should().HaveCount(3);
+        rankingArgs.Entries.Should().BeSameAs(expected);
+        rankingArgs.Entries.Should().ContainSingle().Which.Should().Be(expected[0]);
     }
-
     #endregion Users
 
 }

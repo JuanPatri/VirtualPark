9ded3d5020a213afa124dd37e9bdba4da1ff9e1f
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-09-29 21:35:27 -0300

    GREEN: visit registration service create success

--- src/VirtualPark.BusinessLogic/VisitRegistrations/Service/VisitRegistrationService.cs
+++ src/VirtualPark.BusinessLogic/VisitRegistrations/Service/VisitRegistrationService.cs
@@ -6,6 +6,37 @@ using VirtualPark.Repository;
 
 namespace VirtualPark.BusinessLogic.VisitRegistrations.Service;
 
-public class VisitRegistrationService()
+public class VisitRegistrationService(IRepository<VisitRegistration> visitRegistrationRepository, IReadOnlyRepository<VisitorProfile> visitorProfileRepository, IReadOnlyRepository<Attraction> attractionRepository)
 {
+    private readonly IRepository<VisitRegistration> _visitRegistrationRepository = visitRegistrationRepository;
+    private readonly IReadOnlyRepository<VisitorProfile> _visitorProfileRepository = visitorProfileRepository;
+    private readonly IReadOnlyRepository<Attraction> _attractionRepository = attractionRepository;
+    public VisitRegistration Create(VisitRegistrationArgs args)
+    {
+        var entity = MapToEntity(args);
+
+        _visitRegistrationRepository.Add(entity);
+
+        return entity;
+    }
+
+    private VisitRegistration MapToEntity(VisitRegistrationArgs args)
+    {
+        var visitor = _visitorProfileRepository.Get(v => v.Id == args.VisitorProfileId);
+
+        List<Attraction> attractions = new List<Attraction>();
+        foreach(var attractionId in args.AttractionsId)
+        {
+            var attraction = _attractionRepository.Get(x => x.Id == attractionId);
+
+            attractions.Add(attraction!);
+        }
+
+        return new VisitRegistration
+        {
+            VisitorId = visitor.Id,
+            Visitor = visitor,
+            Attractions = attractions
+        };
+    }
 }

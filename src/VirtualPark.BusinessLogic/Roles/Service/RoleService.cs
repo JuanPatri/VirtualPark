2e9c458ab0a24628ba356e6551f9ea0d93dcb44f
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-01 20:24:31 -0300

    GREEN: GuidToPermission_WithValidIds_ReturnsPermissionsInOrder

--- src/VirtualPark.BusinessLogic/Roles/Service/RoleService.cs
+++ src/VirtualPark.BusinessLogic/Roles/Service/RoleService.cs
@@ -7,4 +7,15 @@ namespace VirtualPark.BusinessLogic.Roles.Service;
 public sealed class RoleService(IReadOnlyRepository<Permission> permissionReadOnlyRepositor)
 {
     private readonly IReadOnlyRepository<Permission> _permissionReadOnlyRepositor = permissionReadOnlyRepositor;
+
+    public List<Permission> GuidToPermission(List<Guid> permissionIds)
+    {
+        ArgumentNullException.ThrowIfNull(permissionIds);
+
+        List<Permission> permissions = permissionIds.Select(guid =>
+            _permissionReadOnlyRepositor.Get(p => p.Id == guid) ??
+            throw new KeyNotFoundException($"Permission with id {guid} does not exist")).ToList();
+
+        return permissions;
+    }
 }

fac878f843cdf9e60d5f76dde8ebbc78adf8d9c2
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-01 19:46:11 -0300

    REFACTR: Name_Getter_ReturnsAssignedValue

--- src/VirtualPark.BusinessLogic/Roles/Models/RoleArgs.cs
+++ src/VirtualPark.BusinessLogic/Roles/Models/RoleArgs.cs
@@ -4,5 +4,5 @@ namespace VirtualPark.BusinessLogic.Roles.Models;
 
 public sealed class RoleArgs(string name)
 {
-    public string Name { get; set; } = ValidationServices.ValidateNullOrEmpty(name);
+    public string Name { get; init; } = ValidationServices.ValidateNullOrEmpty(name);
 }

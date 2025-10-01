e8d6138138a922e06b688a3713b90fdad7503d4f
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-01 19:48:48 -0300

    REFACTOR: Description_Getter_ReturnsAssignedValue

--- src/VirtualPark.BusinessLogic/Roles/Models/RoleArgs.cs
+++ src/VirtualPark.BusinessLogic/Roles/Models/RoleArgs.cs
@@ -5,5 +5,5 @@ namespace VirtualPark.BusinessLogic.Roles.Models;
 public sealed class RoleArgs(string name, string description)
 {
     public string Name { get; init; } = ValidationServices.ValidateNullOrEmpty(name);
-    public string Description { get; set; } = ValidationServices.ValidateNullOrEmpty(description);
+    public string Description { get; init; } = ValidationServices.ValidateNullOrEmpty(description);
 }

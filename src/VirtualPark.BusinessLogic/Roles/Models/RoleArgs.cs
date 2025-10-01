ae7b28ea91929d70a39b2922003fbcffc94fae0e
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-01 19:48:24 -0300

    GREEN: Description_Getter_ReturnsAssignedValue

--- src/VirtualPark.BusinessLogic/Roles/Models/RoleArgs.cs
+++ src/VirtualPark.BusinessLogic/Roles/Models/RoleArgs.cs
@@ -2,7 +2,8 @@ using VirtualPark.BusinessLogic.Validations.Services;
 
 namespace VirtualPark.BusinessLogic.Roles.Models;
 
-public sealed class RoleArgs(string name)
+public sealed class RoleArgs(string name, string description)
 {
     public string Name { get; init; } = ValidationServices.ValidateNullOrEmpty(name);
+    public string Description { get; set; } = ValidationServices.ValidateNullOrEmpty(description);
 }

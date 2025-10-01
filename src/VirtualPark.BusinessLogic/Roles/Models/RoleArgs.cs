5184a98981d9ddd2421b0bdda2f9e1a40dd653e3
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-01 19:55:18 -0300

    GREEN: PermissionIds_Getter_ReturnsParsedGuids

--- src/VirtualPark.BusinessLogic/Roles/Models/RoleArgs.cs
+++ src/VirtualPark.BusinessLogic/Roles/Models/RoleArgs.cs
@@ -2,8 +2,9 @@ using VirtualPark.BusinessLogic.Validations.Services;
 
 namespace VirtualPark.BusinessLogic.Roles.Models;
 
-public sealed class RoleArgs(string name, string description)
+public sealed class RoleArgs(string name, string description, string [] permissions)
 {
     public string Name { get; init; } = ValidationServices.ValidateNullOrEmpty(name);
     public string Description { get; init; } = ValidationServices.ValidateNullOrEmpty(description);
+    public List<Guid> PermissionIds { get; init; } = permissions.Select(ValidationServices.ValidateAndParseGuid).ToList();
 }

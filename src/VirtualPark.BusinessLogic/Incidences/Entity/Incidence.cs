7c573b6c6e3c33315f271e2b5e931e13959fd0b5
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-03 19:29:43 -0300

    GREEN: incidence attraction getter

--- src/VirtualPark.BusinessLogic/Incidences/Entity/Incidence.cs
+++ src/VirtualPark.BusinessLogic/Incidences/Entity/Incidence.cs
@@ -1,3 +1,4 @@
+using VirtualPark.BusinessLogic.Attractions.Entity;
 using VirtualPark.BusinessLogic.TypeIncidences.Entity;
 
 namespace VirtualPark.BusinessLogic.Incidences.Entity;
@@ -10,6 +11,7 @@ public sealed class Incidence
     public string Description { get; set; } = null!;
     public DateTime Start { get; set; }
     public DateTime End { get; set; }
+    public Attraction Attraction { get; init; } = null!;
     public Guid AttractionId { get; set; }
     public bool Active { get; set; }
 }

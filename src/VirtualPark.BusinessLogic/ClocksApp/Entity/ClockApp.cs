5909794b921ad3da44af1f392b91d08f4ad500ff
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-03 20:20:03 -0300

    GREEN DateSystem_Create_ShouldReturnDateTimeNow

--- src/VirtualPark.BusinessLogic/ClocksApp/Entity/ClockApp.cs
+++ src/VirtualPark.BusinessLogic/ClocksApp/Entity/ClockApp.cs
@@ -3,5 +3,5 @@ namespace VirtualPark.BusinessLogic.ClocksApp.Entity;
 public sealed class ClockApp
 {
     public Guid Id { get; init; } = Guid.NewGuid();
-    public DateTime DateSystem { get; set; }
+    public DateTime DateSystem { get; set; } = DateTime.Now;
 }

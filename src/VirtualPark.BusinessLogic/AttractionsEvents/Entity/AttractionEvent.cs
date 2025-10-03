8f3df30d714ed97d8f6ac4f838be677189709d01
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-03 17:54:33 -0300

    GREEN: attractionEvent attractionId should be settable

--- src/VirtualPark.BusinessLogic/AttractionsEvents/Entity/AttractionEvent.cs
+++ src/VirtualPark.BusinessLogic/AttractionsEvents/Entity/AttractionEvent.cs
@@ -2,5 +2,5 @@ namespace VirtualPark.BusinessLogic.AttractionsEvents.Entity;
 
 public class AttractionEvent
 {
-    public Guid AttractionId { get; init; }
+    public Guid AttractionId { get; set; }
 }

03e5743dfe7ccd44f9b03eb2363c5b6a8de23bdb
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-05 19:40:48 -0300

    create attraction configuration

--- src/VirtualPark.DataAccess/SqlContext.cs
+++ src/VirtualPark.DataAccess/SqlContext.cs
@@ -35,25 +35,9 @@ public class SqlContext(DbContextOptions<SqlContext> options) : DbContext(option
     {
         base.OnModelCreating(modelBuilder);
 
-        modelBuilder.Entity<Event>(entity =>
-        {
-            
-        });
-
         modelBuilder.Entity<Attraction>(entity =>
         {
-            entity.ToTable("Attractions");
 
-            entity.HasKey(a => a.Id);
-            entity.Property(a => a.Id).ValueGeneratedNever();
-
-            entity.Property(a => a.Type).IsRequired();
-            entity.Property(a => a.Name).IsRequired();
-            entity.Property(a => a.MiniumAge).IsRequired();
-            entity.Property(a => a.Capacity).IsRequired();
-            entity.Property(a => a.Description).IsRequired();
-            entity.Property(a => a.CurrentVisitors).HasDefaultValue(0);
-            entity.Property(a => a.Available).HasDefaultValue(true);
         });
 
         modelBuilder.Entity<TypeIncidence>(entity =>

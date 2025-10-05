9d3cf0f66c73f37fa663ea8b18e1e36ed32d305f
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-05 17:38:33 -0300

    REFACTOR: createVisitorProfileRequest DateOfBirth Getter

--- tests/VirtualPark.WebApi.Test/Controllers/VisitorsProfile/ModelsIn/CreateVisitorProfileRequestTest.cs
+++ tests/VirtualPark.WebApi.Test/Controllers/VisitorsProfile/ModelsIn/CreateVisitorProfileRequestTest.cs
@@ -8,11 +8,13 @@ namespace VirtualPark.WebApi.Test.Controllers.VisitorsProfile.ModelsIn;
 [TestCategory("CreateVisitorProfileRequest")]
 public class CreateVisitorProfileRequestTest
 {
+    #region DateOfBirth
     [TestMethod]
     [TestCategory("Validation")]
-    public void DateOfBirth_Getter()
+    public void DateOfBirth_Getter_ReturnsAssignedValue()
     {
         var createVisitorProfileRequest = new CreateVisitorProfileRequest { DateOfBirth = "2002-07-30" };
         createVisitorProfileRequest.DateOfBirth.Should().Be("2002-07-30");
     }
+    #endregion
 }

f1ac427553e4c2502ee57c22a9b2552e5e24093f
gdianesi <gd329180@fi365.ort.edu.uy>
2025-10-06 09:12:08 -0300

    GREEN: toArgs method

--- src/VirtualPark.WebApi/Controllers/Events/ModelsIn/CreateEventRequest.cs
+++ src/VirtualPark.WebApi/Controllers/Events/ModelsIn/CreateEventRequest.cs
@@ -1,3 +1,6 @@
+using VirtualPark.BusinessLogic.Events.Models;
+using VirtualPark.BusinessLogic.Validations.Services;
+
 namespace VirtualPark.WebApi.Controllers.Events.ModelsIn;
 
 public sealed class CreateEventRequest
@@ -7,4 +10,14 @@ public sealed class CreateEventRequest
     public string? Cost { get; init; }
     public string? Date { get; init; }
     public string? Name { get; init; }
+
+    public EventsArgs ToArgs()
+    {
+        return new EventsArgs(
+            ValidationServices.ValidateNullOrEmpty(Name),
+            ValidationServices.ValidateNullOrEmpty(Date),
+            ValidationServices.ValidatePositive(int.Parse(ValidationServices.ValidateNullOrEmpty(Capacity))),
+            ValidationServices.ValidatePositive(int.Parse(ValidationServices.ValidateNullOrEmpty(Cost))),
+            AttractionsIds ?? throw new ArgumentException("Attractions list cannot be null"));
+    }
 }

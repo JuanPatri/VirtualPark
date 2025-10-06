3b5efb0f964cb7f41c535c2d953429cd632fd72f
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-06 00:32:41 -0300

    GREEN: CreateAttractionRequest_EventsProperty_GetAndSet_ShouldWorkCorrectly

--- src/VirtualPark.WebApi/Controllers/Attractions/ModelsIn/CreateAttractionRequest.cs
+++ src/VirtualPark.WebApi/Controllers/Attractions/ModelsIn/CreateAttractionRequest.cs
@@ -7,4 +7,5 @@ public sealed class CreateAttractionRequest
     public string? MiniumAge { get; init; }
     public string? Capacity { get; init; }
     public string? Description { get; init; }
+    public List<string>? EventIds { get; set; }
 }

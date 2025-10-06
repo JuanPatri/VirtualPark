65f9e927153d2f82178ba589fd068cc51f5b2001
gdianesi <gd329180@fi365.ort.edu.uy>
2025-10-06 09:35:43 -0300

    REFACTOR: id getter should return assigned value

--- src/VirtualPark.WebApi/Controllers/Events/ModelsOut/GetEventResponse.cs
+++ src/VirtualPark.WebApi/Controllers/Events/ModelsOut/GetEventResponse.cs
@@ -1,6 +1,6 @@
 namespace VirtualPark.WebApi.Controllers.Events.ModelsOut;
 
-public class GetEventResponse(string id)
+public sealed class GetEventResponse(string id)
 {
     public string Id { get; } = id;
 }

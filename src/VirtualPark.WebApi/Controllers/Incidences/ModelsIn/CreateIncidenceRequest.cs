0c0502c6539d85dbbdd7b06896fc43ae216e4d6c
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-06 20:51:47 -0300

    REFACTOR: End_Getter_ReturnsAssignedValue

--- src/VirtualPark.WebApi/Controllers/Incidences/ModelsIn/CreateIncidenceRequest.cs
+++ src/VirtualPark.WebApi/Controllers/Incidences/ModelsIn/CreateIncidenceRequest.cs
@@ -5,5 +5,5 @@ public class CreateIncidenceRequest
     public string? TypeId { get; init; }
     public string? Description { get; init; }
     public string? Start { get; init; }
-    public string? End { get; set; }
+    public string? End { get; init; }
 }

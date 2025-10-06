3f6d27eda10d23981a1f843349b04a6ad8ea2f6d
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-06 20:56:30 -0300

    REFACTOR: Active_Getter_ReturnsAssignedValue

--- src/VirtualPark.WebApi/Controllers/Incidences/ModelsIn/CreateIncidenceRequest.cs
+++ src/VirtualPark.WebApi/Controllers/Incidences/ModelsIn/CreateIncidenceRequest.cs
@@ -7,5 +7,5 @@ public class CreateIncidenceRequest
     public string? Start { get; init; }
     public string? End { get; init; }
     public string? AttractionId { get; init; }
-    public string? Active { get; set; }
+    public string? Active { get; init; }
 }

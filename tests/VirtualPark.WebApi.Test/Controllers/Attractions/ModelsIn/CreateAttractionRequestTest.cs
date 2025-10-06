0027f14b1988a2045c98d2b47e3efcf3ef683408
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-06 00:10:52 -0300

    RED: CreateAttractionRequest_NameProperty_GetAndSet_ShouldWorkCorrectly

--- tests/VirtualPark.WebApi.Test/Controllers/Attractions/ModelsIn/CreateAttractionRequestTest.cs
+++ tests/VirtualPark.WebApi.Test/Controllers/Attractions/ModelsIn/CreateAttractionRequestTest.cs
@@ -1,6 +1,16 @@
+using FluentAssertions;
+
 namespace VirtualPark.WebApi.Test.Controllers.Attractions.ModelsIn;
 
 public class CreateAttractionRequestTest
 {
-    
+    #region Name
+
+    [TestMethod]
+    public void CreateAttractionRequest_NameProperty_GetAndSet_ShouldWorkCorrectly()
+    {
+        var attraction = new CreateAttractionRequestTest { Name = "Titanic" };
+        attraction.Name.Should().Be("Titanic");
+    }
+    #endregion
 }

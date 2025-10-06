b7052fb799f89fce39320844dd13ffb79c31c9cf
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-06 00:24:43 -0300

    REFACTOR : CreateAttractionRequest_CapacityProperty_GetAndSet_ShouldWorkCorrectly

--- src/VirtualPark.WebApi/Controllers/Attractions/ModelsIn/CreateAttractionRequest.cs
+++ src/VirtualPark.WebApi/Controllers/Attractions/ModelsIn/CreateAttractionRequest.cs
@@ -5,5 +5,5 @@ public sealed class CreateAttractionRequest
     public string? Name { get; init; }
     public string? TypeId { get; init; }
     public string? MiniumAge { get; init; }
-    public string? Capacity { get; set; }
+    public string? Capacity { get; init; }
 }

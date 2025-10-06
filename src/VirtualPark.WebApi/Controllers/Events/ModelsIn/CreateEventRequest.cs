2446abac7d330a1912e365109eabbfd1c8aa5e27
gdianesi <gd329180@fi365.ort.edu.uy>
2025-10-06 08:53:13 -0300

    GREEN: createEventRequest should return value date assigned

--- src/VirtualPark.WebApi/Controllers/Events/ModelsIn/CreateEventRequest.cs
+++ src/VirtualPark.WebApi/Controllers/Events/ModelsIn/CreateEventRequest.cs
@@ -3,4 +3,5 @@ namespace VirtualPark.WebApi.Controllers.Events.ModelsIn;
 public sealed class CreateEventRequest
 {
     public string? Name { get; init; }
+    public string? Date { get; init; }
 }

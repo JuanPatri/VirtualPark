11eff3e7bf85b085e155a1b34002ce97eda0ffa0
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-06 20:42:40 -0300

    RED: Name_Getter_ReturnsAssignedValue

---
+++ tests/VirtualPark.WebApi.Test/Controllers/Incidences/ModelsIn/CreateIncidenceRequestTest.cs
@@ -0,0 +1,18 @@
+using VirtualPark.WebApi.Controllers.Incidences.ModelsIn;
+
+namespace VirtualPark.WebApi.Test.Controllers.Incidences.ModelsIn;
+
+[TestMethod]
+public class CreateIncidenceRequestTest
+{
+    #region Name
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void Name_Getter_ReturnsAssignedValue()
+    {
+        var typeId = Guid.NewGuid().ToString();
+        var createIncidenceRequest = new CreateIncidenceRequest { TypeId = typeId };
+        createIncidenceRequest.TypeId.Should().Be(typeId);
+    }
+    #endregion
+}

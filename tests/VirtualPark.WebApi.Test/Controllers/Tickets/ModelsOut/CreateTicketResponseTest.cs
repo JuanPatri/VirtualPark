284ad0f106836bd273dce6322761a6d9b3875a19
gdianesi <gd329180@fi365.ort.edu.uy>
2025-10-06 20:05:32 -0300

    RED: CreateTicketResponse

---
+++ tests/VirtualPark.WebApi.Test/Controllers/Tickets/ModelsOut/CreateTicketResponseTest.cs
@@ -0,0 +1,21 @@
+namespace VirtualPark.WebApi.Test.Controllers.Tickets.ModelsOut
+{
+    [TestClass]
+    [TestCategory("ModelsOut")]
+    [TestCategory("CreateTicketResponse")]
+    public sealed class CreateTicketResponseTest
+    {
+        #region Id
+        [TestMethod]
+        [TestCategory("Validation")]
+        public void Id_Getter_ReturnsAssignedValue()
+        {
+            var id = Guid.NewGuid().ToString();
+
+            var response = new CreateTicketResponse(id);
+
+            response.Id.Should().Be(id);
+        }
+        #endregion
+    }
+}

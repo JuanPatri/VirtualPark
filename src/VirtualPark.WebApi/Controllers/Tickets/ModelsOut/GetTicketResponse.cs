54f3b9745b955c8be3d970b5c15671c4b3456294
gdianesi <gd329180@fi365.ort.edu.uy>
2025-10-06 20:25:29 -0300

    REFACTOR: getTicketResponse date

--- src/VirtualPark.WebApi/Controllers/Tickets/ModelsOut/GetTicketResponse.cs
+++ src/VirtualPark.WebApi/Controllers/Tickets/ModelsOut/GetTicketResponse.cs
@@ -4,6 +4,6 @@ public sealed class GetTicketResponse(string id, string type, string date)
 {
     public string Id { get; } = id;
     public string Type { get; } = type;
-    public string Date { get; set; } = date;
+    public string Date { get; } = date;
 
 }

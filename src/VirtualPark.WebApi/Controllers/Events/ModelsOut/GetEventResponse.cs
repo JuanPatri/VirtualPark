5e513aa0320de2be2508308e73f8957a26798ee4
gdianesi <gd329180@fi365.ort.edu.uy>
2025-10-06 09:37:22 -0300

    GREEN: name  getter should return assigned value

--- src/VirtualPark.WebApi/Controllers/Events/ModelsOut/GetEventResponse.cs
+++ src/VirtualPark.WebApi/Controllers/Events/ModelsOut/GetEventResponse.cs
@@ -1,6 +1,7 @@
 namespace VirtualPark.WebApi.Controllers.Events.ModelsOut;
 
-public sealed class GetEventResponse(string id)
+public sealed class GetEventResponse(string id, string name)
 {
     public string Id { get; } = id;
+    public string Name { get; } = name;
 }

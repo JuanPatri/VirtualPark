2db4719ace9a63f41bbce7c616e5b334cca61447
gdianesi <gd329180@fi365.ort.edu.uy>
2025-10-06 09:42:57 -0300

    REFACTOR: date getter should return assigned value

--- src/VirtualPark.WebApi/Controllers/Events/ModelsOut/GetEventResponse.cs
+++ src/VirtualPark.WebApi/Controllers/Events/ModelsOut/GetEventResponse.cs
@@ -2,7 +2,7 @@ namespace VirtualPark.WebApi.Controllers.Events.ModelsOut;
 
 public sealed class GetEventResponse(string id, string name, string date)
 {
+    public string Date { get; set; } = date;
     public string Id { get; } = id;
     public string Name { get; } = name;
-    public string Date { get; set; } = date;
 }

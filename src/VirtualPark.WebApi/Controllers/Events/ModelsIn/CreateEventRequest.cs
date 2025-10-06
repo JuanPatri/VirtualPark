4fc218e03a0f2c209035c4160385e41eabe4c1d4
gdianesi <gd329180@fi365.ort.edu.uy>
2025-10-06 09:01:15 -0300

    REFACTOR: createEventRequest should return capacity value assigned

--- src/VirtualPark.WebApi/Controllers/Events/ModelsIn/CreateEventRequest.cs
+++ src/VirtualPark.WebApi/Controllers/Events/ModelsIn/CreateEventRequest.cs
@@ -2,7 +2,7 @@ namespace VirtualPark.WebApi.Controllers.Events.ModelsIn;
 
 public sealed class CreateEventRequest
 {
+    public string? Capacity { get; set; }
     public string? Date { get; init; }
     public string? Name { get; init; }
-    public string? Capacity { get; set; }
 }

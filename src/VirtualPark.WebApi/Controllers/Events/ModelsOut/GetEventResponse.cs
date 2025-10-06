0b82ec43dd42dfab1de9f09deab5de03f6aecf9a
gdianesi <gd329180@fi365.ort.edu.uy>
2025-10-06 09:46:40 -0300

    REFACTOR: capacity getter should return assigned value

--- src/VirtualPark.WebApi/Controllers/Events/ModelsOut/GetEventResponse.cs
+++ src/VirtualPark.WebApi/Controllers/Events/ModelsOut/GetEventResponse.cs
@@ -2,8 +2,8 @@ namespace VirtualPark.WebApi.Controllers.Events.ModelsOut;
 
 public sealed class GetEventResponse(string id, string name, string date, string capacity)
 {
+    public string Capacity { get; } = capacity;
     public string Date { get; set; } = date;
     public string Id { get; } = id;
     public string Name { get; } = name;
-    public string Capacity { get; } = capacity;
 }

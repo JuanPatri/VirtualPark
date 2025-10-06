352cafd1c14f160514892422139aa721caf23828
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-06 00:44:59 -0300

    RED: Id_Getter_ReturnsAssignedValue

--- tests/VirtualPark.WebApi.Test/Controllers/Attractions/ModelsOut/CreateAttractionResponseTest.cs
+++ tests/VirtualPark.WebApi.Test/Controllers/Attractions/ModelsOut/CreateAttractionResponseTest.cs
@@ -1,6 +1,17 @@
 namespace VirtualPark.WebApi.Test.Controllers.Attractions.ModelsOut;
 
+[TestClass]
+[TestCategory("CreateAttractionRespone")]
 public class CreateAttractionResponseTest
 {
-    
+    #region Id
+
+    [TestMethod]
+    public void Id_Getter_ReturnsAssignedValue()
+    {
+        var id = Guid.NewGuid();
+        CreateAttractionResponse attraction = new CreateAttractionResponse { Id = id.ToString() };
+        attraction.Id.Should().Be(id.ToString());
+    }
+    #endregion
 }

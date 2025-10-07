a9aa114f746d2fd3354a546fb70365c46f930565
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-06 21:20:11 -0300

    green: Start_Getter_ReturnsAssignedValue

--- src/VirtualPark.WebApi/Controllers/Incidences/ModelsOut/GetIncidenceResponse.cs
+++ src/VirtualPark.WebApi/Controllers/Incidences/ModelsOut/GetIncidenceResponse.cs
@@ -1,8 +1,9 @@
 namespace VirtualPark.WebApi.Controllers.Incidences.ModelsOut;
 
-public class GetIncidenceResponse(string id, string typeId, string description)
+public class GetIncidenceResponse(string id, string typeId, string description, string start)
 {
     public string Id { get; init; } = id;
     public string TypeId { get; init; } = typeId;
     public string Description { get; init; } = description;
+    public string Start { get; init; } = start;
 }

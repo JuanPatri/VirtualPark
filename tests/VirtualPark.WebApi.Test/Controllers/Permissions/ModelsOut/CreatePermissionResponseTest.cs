7e3711ce9782a1beaac92f7ddd280cc221bfa1f0
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-07 01:15:07 -0300

    RED: CreatePermssionResponse Id

---
+++ tests/VirtualPark.WebApi.Test/Controllers/Permissions/ModelsOut/CreatePermissionResponseTest.cs
@@ -0,0 +1,18 @@
+namespace VirtualPark.WebApi.Test.Controllers.Permissions.ModelsOut;
+
+[TestClass]
+[TestCategory("ModelsOut")]
+[TestCategory("CreatePermissionResponse")]
+public class CreatePermissionResponseTest
+{
+    #region Id
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void Id_Getter_ReturnsAssignedValue()
+    {
+        var id = Guid.NewGuid().ToString();
+        var response = new CreatePermissionResponse(id);
+        response.Id.Should().Be(id);
+    }
+    #endregion
+}

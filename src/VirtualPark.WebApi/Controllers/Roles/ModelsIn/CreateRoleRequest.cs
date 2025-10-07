c74d1cd0eff90bd8deaf9b23e336f07fe5519efe
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-06 23:26:00 -0300

    REFACTOR permissions

--- src/VirtualPark.WebApi/Controllers/Roles/ModelsIn/CreateRoleRequest.cs
+++ src/VirtualPark.WebApi/Controllers/Roles/ModelsIn/CreateRoleRequest.cs
@@ -7,5 +7,5 @@ public class CreateRoleRequest
 {
     public string? Name { get; init; }
     public string? Description { get; init; }
-    public List<string>? Permissions { get; set; }
+    public List<string>? Permissions { get; init; }
 }

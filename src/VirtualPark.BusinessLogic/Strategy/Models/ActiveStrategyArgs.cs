a6386268e2810d52596d1892a44dac76719cbfe3
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-07 20:04:52 -0300

    GREEN strategy args

--- src/VirtualPark.BusinessLogic/Strategy/Models/ActiveStrategyArgs.cs
+++ src/VirtualPark.BusinessLogic/Strategy/Models/ActiveStrategyArgs.cs
@@ -4,4 +4,6 @@ namespace VirtualPark.BusinessLogic.Strategy.Models;
 
 public class ActiveStrategyArgs(string strategyKey, string date)
 {
+    public string StrategyKey { get; set; } = ValidationServices.ValidateNullOrEmpty(strategyKey);
+    public DateOnly Date { get; set; } = ValidationServices.ValidateDateOnly(date);
 }

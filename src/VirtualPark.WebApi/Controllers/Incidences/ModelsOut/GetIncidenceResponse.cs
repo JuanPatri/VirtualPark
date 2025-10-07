df0ef2645bd3d1cbd059cdf159fb0b94b573c097
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-06 21:18:06 -0300

    GREEN: TypeId_Getter_ReturnsAssignedValue

--- src/VirtualPark.WebApi/Controllers/Incidences/ModelsOut/GetIncidenceResponse.cs
+++ src/VirtualPark.WebApi/Controllers/Incidences/ModelsOut/GetIncidenceResponse.cs
@@ -1,7 +1,7 @@
 namespace VirtualPark.WebApi.Controllers.Incidences.ModelsOut;
 
-public class GetIncidenceResponse(string id)
+public class GetIncidenceResponse(string id, string typeId)
 {
     public string Id { get; init; } = id;
-    
+    public string TypeId { get; init; } = typeId;
 }

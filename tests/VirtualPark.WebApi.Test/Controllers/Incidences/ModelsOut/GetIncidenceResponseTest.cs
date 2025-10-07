0566e719e4a94394e91edaea70f9f9626b86e3d0
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-06 21:11:25 -0300

    RED: Id_Getter_ReturnsAssignedValue

---
+++ tests/VirtualPark.WebApi.Test/Controllers/Incidences/ModelsOut/GetIncidenceResponseTest.cs
@@ -0,0 +1,17 @@
+using VirtualPark.WebApi.Controllers.Incidences.ModelsOut;
+
+namespace VirtualPark.WebApi.Test.Controllers.Incidences.ModelsOut;
+[TestClass]
+public class GetIncidenceResponseTest
+{
+    #region Id
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void Id_Getter_ReturnsAssignedValue()
+    {
+        var id = Guid.NewGuid().ToString();
+        var response = new GetIncidenceResponse(id);
+        response.Id.Should().Be(id);
+    }
+    #endregion
+}

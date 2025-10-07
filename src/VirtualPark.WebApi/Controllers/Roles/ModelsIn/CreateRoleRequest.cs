a9c0b652c1aa4ddb196d6a911464a52e5f8552f5
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-06 23:38:59 -0300

    REFACTOR CreateRoleRequestTest

--- src/VirtualPark.WebApi/Controllers/Roles/ModelsIn/CreateRoleRequest.cs
+++ src/VirtualPark.WebApi/Controllers/Roles/ModelsIn/CreateRoleRequest.cs
@@ -7,5 +7,12 @@ public class CreateRoleRequest
 {
     public string? Name { get; init; }
     public string? Description { get; init; }
-    public List<string>? Permissions { get; init; }
+    public List<string>? PermissionsIds { get; init; }
+
+    public RoleArgs? ToArgs()
+    {
+        return new RoleArgs(ValidationServices.ValidateNullOrEmpty(Name),
+            ValidationServices.ValidateNullOrEmpty(Description),
+            ValidationServices.ValidateList(PermissionsIds));
+    }
 }
--- tests/VirtualPark.WebApi.Test/Controllers/Roles/ModelsIn/CreateRoleRequestTest.cs
+++ tests/VirtualPark.WebApi.Test/Controllers/Roles/ModelsIn/CreateRoleRequestTest.cs
@@ -4,6 +4,8 @@ using VirtualPark.WebApi.Controllers.Roles.ModelsIn;
 namespace VirtualPark.WebApi.Test.Controllers.Roles.ModelsIn;
 
 [TestClass]
+[TestCategory("ModelsIn")]
+[TestCategory("CreateRoleRequest")]
 public class CreateRoleRequestTest
 {
     #region Name
@@ -11,153 +13,133 @@ public class CreateRoleRequestTest
     [TestCategory("Validation")]
     public void Name_Getter_ReturnsAssignedValue()
     {
-        var request = new CreateRoleRequest { Name = "Admin" };
-        request.Name.Should().Be("Admin");
+        var request = new CreateRoleRequest { Name = "Manager" };
+        request.Name.Should().Be("Manager");
     }
     #endregion
+
     #region Description
     [TestMethod]
     [TestCategory("Validation")]
     public void Description_Getter_ReturnsAssignedValue()
     {
-        var request = new CreateRoleRequest { Description = "Full access to system" };
-        request.Description.Should().Be("Full access to system");
+        var request = new CreateRoleRequest { Description = "Manage users and content" };
+        request.Description.Should().Be("Manage users and content");
     }
     #endregion
-    #region Permissions
+
+    #region PermissionIds (strings de Guid en el request)
     [TestMethod]
     [TestCategory("Validation")]
-    public void Permissions_Getter_ReturnsAssignedValue()
+    public void PermissionIds_Getter_ReturnsAssignedValue()
     {
-        var perms = new List<string> { "users.read", "users.write" };
-        var request = new CreateRoleRequest { Permissions = perms };
+        var g1 = Guid.NewGuid().ToString();
+        var g2 = Guid.NewGuid().ToString();
 
-        request.Permissions.Should().Contain(new[] { "users.read", "users.write" });
+        var request = new CreateRoleRequest { PermissionsIds = [g1, g2] };
+
+        request.PermissionsIds.Should().Contain([g1, g2]);
     }
     #endregion
+
     #region ToArgs
-        #region Success
-        [TestMethod]
-        [TestCategory("Validation")]
-        public void ToArgs_ShouldMapAllFields_WhenInputIsValid()
+    #region Success
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void ToArgs_ShouldMapAllFields_WhenInputIsValid()
+    {
+        var g1 = Guid.NewGuid().ToString();
+        var g2 = Guid.NewGuid().ToString();
+        var g3 = Guid.NewGuid().ToString();
+
+        var request = new CreateRoleRequest
         {
-            var request = new CreateRoleRequest
-            {
-                Name = "Manager",
-                Description = "Manage users and content",
-                Permissions = new List<string> { "users.read", "users.update", "content.publish" }
-            };
+            Name = "Manager",
+            Description = "Manage users and content",
+            PermissionsIds = [g1, g2, g3]
+        };
 
-            var args = request.ToArgs();
+        var args = request.ToArgs(); // ¡invocar!
 
-            args.Should().NotBeNull();
-            args!.Name.Should().Be("Manager");
-            args.Description.Should().Be("Manage users and content");
-            args.Permissions.Should().Contain(new[] { "users.read", "users.update", "content.publish" });
-            args.Permissions.Should().HaveCount(3);
-        }
-        #endregion
+        args.Should().NotBeNull();
+        args.Name.Should().Be("Manager");
+        args.Description.Should().Be("Manage users and content");
 
-        #region Failure
-        [TestMethod]
-        [TestCategory("Validation")]
-        public void ToArgs_ShouldThrow_WhenPermissionsIsNull()
+        args.PermissionIds.Should().HaveCount(3);
+        args.PermissionIds.Should().Contain(Guid.Parse(g1));
+        args.PermissionIds.Should().Contain(Guid.Parse(g2));
+        args.PermissionIds.Should().Contain(Guid.Parse(g3));
+    }
+    #endregion
+
+    #region Failure
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void ToArgs_ShouldThrow_WhenPermissionIdsIsEmpty()
+    {
+        var request = new CreateRoleRequest
         {
-            var request = new CreateRoleRequest
-            {
-                Name = "Reporter",
-                Description = "Read-only access",
-                Permissions = null
-            };
+            Name = "Reporter",
+            Description = "Read-only access",
+            PermissionsIds = []
+        };
 
-            Action act = () => request.ToArgs();
+        Action act = () => request.ToArgs();
 
-            act.Should()
-               .Throw<InvalidOperationException>()
-               .WithMessage("Permission list can't be null");
-        }
+        act.Should()
+            .Throw<InvalidOperationException>()
+            .WithMessage("Role list can't be null");
+    }
 
-        [TestMethod]
-        [TestCategory("Validation")]
-        public void ToArgs_ShouldThrow_WhenPermissionsIsEmpty()
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void ToArgs_ShouldThrow_WhenPermissionIdsIsNull()
+    {
+        var request = new CreateRoleRequest
         {
-            var request = new CreateRoleRequest
-            {
-                Name = "Reporter",
-                Description = "Read-only access",
-                Permissions = new List<string>()
-            };
+            Name = "Reporter",
+            Description = "Read-only access",
+            PermissionsIds = null
+        };
 
-            Action act = () => request.ToArgs();
+        Action act = () => request.ToArgs();
 
-            act.Should()
-               .Throw<InvalidOperationException>()
-               .WithMessage("Permission list can't be null");
-        }
+        act.Should()
+            .Throw<InvalidOperationException>()
+            .WithMessage("Role list can't be null");
+    }
 
-        [TestMethod]
-        [TestCategory("Validation")]
-        public void ToArgs_ShouldThrow_WhenNameIsNull()
-        {
-            var request = new CreateRoleRequest
-            {
-                Name = null,
-                Description = "Some description",
-                Permissions = new List<string> { "users.read" }
-            };
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void ToArgs_ShouldThrow_WhenNameIsNullOrEmpty()
+    {
+        var okId = Guid.NewGuid().ToString();
 
-            Action act = () => request.ToArgs();
+        var nullName = new CreateRoleRequest { Name = null, Description = "desc", PermissionsIds = [okId] };
+        var emptyName = new CreateRoleRequest { Name = string.Empty, Description = "desc", PermissionsIds = [okId] };
 
-            act.Should().Throw<ArgumentException>();
-        }
+        Action act1 = () => nullName.ToArgs();
+        Action act2 = () => emptyName.ToArgs();
 
-        [TestMethod]
-        [TestCategory("Validation")]
-        public void ToArgs_ShouldThrow_WhenNameIsEmpty()
-        {
-            var request = new CreateRoleRequest
-            {
-                Name = string.Empty,
-                Description = "Some description",
-                Permissions = new List<string> { "users.read" }
-            };
+        act1.Should().Throw<ArgumentException>();
+        act2.Should().Throw<ArgumentException>();
+    }
 
-            Action act = () => request.ToArgs();
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void ToArgs_ShouldThrow_WhenDescriptionIsNullOrEmpty()
+    {
+        var okId = Guid.NewGuid().ToString();
 
-            act.Should().Throw<ArgumentException>();
-        }
+        var nullDesc = new CreateRoleRequest { Name = "Role", Description = null, PermissionsIds = [okId] };
+        var emptyDesc = new CreateRoleRequest { Name = "Role", Description = string.Empty, PermissionsIds = [okId] };
 
-        [TestMethod]
-        [TestCategory("Validation")]
-        public void ToArgs_ShouldThrow_WhenDescriptionIsNull()
-        {
-            var request = new CreateRoleRequest
-            {
-                Name = "Operator",
-                Description = null,
-                Permissions = new List<string> { "users.read" }
-            };
+        Action act1 = () => nullDesc.ToArgs();
+        Action act2 = () => emptyDesc.ToArgs();
 
-            Action act = () => request.ToArgs();
-
-            act.Should().Throw<ArgumentException>();
-        }
-
-        [TestMethod]
-        [TestCategory("Validation")]
-        public void ToArgs_ShouldThrow_WhenDescriptionIsEmpty()
-        {
-            var request = new CreateRoleRequest
-            {
-                Name = "Operator",
-                Description = string.Empty,
-                Permissions = new List<string> { "users.read" }
-            };
-
-            Action act = () => request.ToArgs();
-
-            act.Should().Throw<ArgumentException>();
-        }
-        #endregion
-        #endregion
+        act1.Should().Throw<ArgumentException>();
+        act2.Should().Throw<ArgumentException>();
+    }
+    #endregion
+    #endregion
 }

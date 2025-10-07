bce7ac79aa183eadd84f3ef7860927ac8ec209ad
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-07 01:08:07 -0300

    RED: CreatePermssionRequest toArgs, and REFACTOR: permission args

--- tests/VirtualPark.WebApi.Test/Controllers/Permissions/ModelsIn/CreatePermissionRequestTest.cs
+++ tests/VirtualPark.WebApi.Test/Controllers/Permissions/ModelsIn/CreatePermissionRequestTest.cs
@@ -28,6 +28,7 @@ public class CreatePermissionRequestTest
     }
     #endregion
 
+    #region RolesIds
     [TestMethod]
     [TestCategory("Validation")]
     public void Roles_Getter_ReturnsAssignedValue()
@@ -36,4 +37,119 @@ public class CreatePermissionRequestTest
         var createPermissionRequest = new CreatePermissionRequest { RolesIds = [guid] };
         createPermissionRequest.RolesIds.Should().Contain([guid]);
     }
+    #endregion
+
+    #region ToArgs
+    #region Success
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void ToArgs_ShouldMapAllFields_AndConvertRolesToGuids()
+    {
+        var guid1 = Guid.NewGuid().ToString();
+        var guid2 = Guid.NewGuid().ToString();
+
+        var request = new CreatePermissionRequest
+        {
+            Description = "Manage Users",
+            Key = "USERS_MANAGE",
+            RolesIds = [guid1, guid2]
+        };
+
+        var result = request.ToArgs();
+
+        result.Should().NotBeNull();
+        result.Description.Should().Be("Manage Users");
+        result.Key.Should().Be("USERS_MANAGE");
+        result.Roles.Should().HaveCount(2);
+        result.Roles.Should().Contain(Guid.Parse(guid1));
+        result.Roles.Should().Contain(Guid.Parse(guid2));
+    }
+    #endregion
+
+    #region Failure
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void ToArgs_ShouldThrow_WhenDescriptionIsNullOrEmpty()
+    {
+        var request = new CreatePermissionRequest
+        {
+            Description = null,
+            Key = "KEY",
+            RolesIds = [Guid.NewGuid().ToString()]
+        };
+
+        Action act = () => request.ToArgs();
+
+        act.Should().Throw<ArgumentException>()
+            .WithMessage("Value cannot be null or empty.*");
+    }
+
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void ToArgs_ShouldThrow_WhenKeyIsNullOrEmpty()
+    {
+        var request = new CreatePermissionRequest
+        {
+            Description = "Desc",
+            Key = string.Empty,
+            RolesIds = [Guid.NewGuid().ToString()]
+        };
+
+        Action act = () => request.ToArgs();
+
+        act.Should().Throw<ArgumentException>()
+            .WithMessage("Value cannot be null or empty.*");
+    }
+
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void ToArgs_ShouldThrow_WhenRolesIdsIsNull()
+    {
+        var request = new CreatePermissionRequest
+        {
+            Description = "Desc",
+            Key = "Key",
+            RolesIds = null
+        };
+
+        Action act = () => request.ToArgs();
+
+        act.Should().Throw<InvalidOperationException>()
+            .WithMessage("List can't be null or empty*");
+    }
+
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void ToArgs_ShouldThrow_WhenRolesIdsIsEmpty()
+    {
+        var request = new CreatePermissionRequest
+        {
+            Description = "Desc",
+            Key = "Key",
+            RolesIds = []
+        };
+
+        Action act = () => request.ToArgs();
+
+        act.Should().Throw<InvalidOperationException>()
+            .WithMessage("List can't be null or empty*");
+    }
+
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void ToArgs_ShouldThrow_WhenRolesIdsContainInvalidGuid()
+    {
+        var request = new CreatePermissionRequest
+        {
+            Description = "Desc",
+            Key = "Key",
+            RolesIds = ["invalid-guid"]
+        };
+
+        Action act = () => request.ToArgs();
+
+        act.Should().Throw<FormatException>();
+    }
+    #endregion
+    #endregion
 }

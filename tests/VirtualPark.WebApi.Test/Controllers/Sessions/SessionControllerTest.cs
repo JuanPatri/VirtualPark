d24b9c46c9181ec874a1e3ca91e870e02c2443ac
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-07 02:36:23 -0300

    RED: SesionController logIN

--- tests/VirtualPark.WebApi.Test/Controllers/Sessions/SessionControllerTest.cs
+++ tests/VirtualPark.WebApi.Test/Controllers/Sessions/SessionControllerTest.cs
@@ -10,4 +10,37 @@ namespace VirtualPark.WebApi.Test.Controllers.Sessions;
 [TestClass]
 public class SessionControllerTest
 {
+    private Mock<ISessionService> _sessionServiceMock = null!;
+    private SessionController _sessionController = null!;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        _sessionServiceMock = new Mock<ISessionService>(MockBehavior.Strict);
+        _sessionController = new SessionController(_sessionServiceMock.Object);
+    }
+
+    #region LogIn
+    [TestMethod]
+    public void LogIn_ValidInput_ReturnsTokenResponse()
+    {
+        var userId = Guid.NewGuid().ToString();
+        var request = new LogInSessionRequest { UserId = userId };
+
+        var expectedArgs = request.ToArgs();
+        var token = Guid.NewGuid();
+
+        _sessionServiceMock
+            .Setup(s => s.LogIn(It.Is<SessionArgs>(a => a.UserId == expectedArgs.UserId)))
+            .Returns(token);
+
+        var response = _sessionController.LogIn(request);
+
+        response.Should().NotBeNull();
+        response.Should().BeOfType<LogInSessionResponse>();
+        response.Token.Should().Be(token.ToString());
+
+        _sessionServiceMock.VerifyAll();
+    }
+    #endregion
 }

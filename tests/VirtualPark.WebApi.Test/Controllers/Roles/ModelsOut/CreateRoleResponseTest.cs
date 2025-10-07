fe85fb2bada5b86caafee8d862d777de2d4af527
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-06 23:40:41 -0300

    RED id

--- tests/VirtualPark.WebApi.Test/Controllers/Roles/ModelsOut/CreateRoleResponseTest.cs
+++ tests/VirtualPark.WebApi.Test/Controllers/Roles/ModelsOut/CreateRoleResponseTest.cs
@@ -1,7 +1,19 @@
+using VirtualPark.WebApi.Controllers.Roles.ModelsOut;
+
 namespace VirtualPark.WebApi.Test.Controllers.Roles.ModelsOut;
 
 [TestClass]
 
 public class CreateRoleResponseTest
 {
+    #region Id
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void Id_Getter_ReturnsAssignedValue()
+    {
+        var id = Guid.NewGuid().ToString();
+        var response = new CreateRoleResponse(id);
+        response.Id.Should().Be(id);
+    }
+    #endregion
 }

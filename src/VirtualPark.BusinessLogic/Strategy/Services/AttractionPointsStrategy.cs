8d8ee7b37153d5e41bbd2b311b8c4314b0451c17
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-07 19:25:04 -0300

    green: AttractionPointsStrategy

--- src/VirtualPark.BusinessLogic/Strategy/Services/AttractionPointsStrategy.cs
+++ src/VirtualPark.BusinessLogic/Strategy/Services/AttractionPointsStrategy.cs
@@ -5,4 +5,29 @@ namespace VirtualPark.BusinessLogic.Strategy.Services;
 
 public class AttractionPointsStrategy : IStrategy
 {
+    public string Key { get; } = "Attraction";
+
+    public int CalculatePoints(VisitRegistration visit)
+    {
+        if (visit.Attractions.Count == 0)
+        {
+            return 0;
+        }
+
+        var totalPoints = 0;
+
+        foreach (var attraction in visit.Attractions)
+        {
+            totalPoints += attraction.Type switch
+            {
+                AttractionType.RollerCoaster => 50,
+                AttractionType.Show => 30,
+                AttractionType.Simulator => 20,
+                _ => 10
+            };
+        }
+
+        return totalPoints;
+    }
+
 }

a3d555cb02c988cbdcd1e82b35132e8d19e9eb00
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-08 00:37:48 -0300

    RED: reportAttractionResponse visits

--- tests/VirtualPark.WebApi.Test/Controllers/Attractions/ModelsOut/ReportAttractionsResponseTest.cs
+++ tests/VirtualPark.WebApi.Test/Controllers/Attractions/ModelsOut/ReportAttractionsResponseTest.cs
@@ -12,9 +12,18 @@ public class ReportAttractionsResponseTest
     [TestCategory("Validation")]
     public void Name_Getter_ReturnsAssignedValue()
     {
-        var id = Guid.NewGuid().ToString();
         var response = new ReportAttractionsResponse("Roller");
         response.Name.Should().Be("Roller");
+    }
+    #endregion
+
+    #region Visits
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void Visits_Getter_ReturnsAssignedValue()
+    {
+        var response = new ReportAttractionsResponse("Roller", "1");
+        response.Visits.Should().Be("1");
     }
     #endregion
 }

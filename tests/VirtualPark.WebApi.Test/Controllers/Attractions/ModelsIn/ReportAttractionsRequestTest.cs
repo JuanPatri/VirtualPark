faedd63f439067c267a26aec6e32951c2074d0f3
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-08 00:22:58 -0300

    RED: reportAttractionReques from

--- tests/VirtualPark.WebApi.Test/Controllers/Attractions/ModelsIn/ReportAttractionsRequestTest.cs
+++ tests/VirtualPark.WebApi.Test/Controllers/Attractions/ModelsIn/ReportAttractionsRequestTest.cs
@@ -4,4 +4,13 @@ namespace VirtualPark.WebApi.Test.Controllers.Attractions.ModelsIn;
 [TestCategory("CreateAttractionRequest")]
 public class ReportAttractionsRequestTest
 {
+    #region From
+
+    [TestMethod]
+    public void ReportAttractionRequest_NameProperty_GetAndSet_ShouldWorkCorrectly()
+    {
+        var attraction = new ReportAttractionRequest { From = "2025-04-23" };
+        attraction.From.Should().Be("2025-04-23");
+    }
+    #endregion
 }

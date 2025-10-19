18ba598e27a4bd17c1e8533e583cc868185107f2
gdianesi <gd329180@fi365.ort.edu.uy>
2025-10-19 16:28:34 -0300

    RED: rewardRedemption date

--- tests/VirtualPark.BusinessLogic.Test/RewardRedemption/Entity/RewardRedemptionTest.cs
+++ tests/VirtualPark.BusinessLogic.Test/RewardRedemption/Entity/RewardRedemptionTest.cs
@@ -33,4 +33,17 @@ public sealed class RewardRedemptionTest
         redemption.VisitorId.Should().Be(visitorId);
     }
     #endregion
+
+    [TestMethod]
+    [TestCategory("Success")]
+    public void Constructor_WhenDateIsValid_ShouldSetDate()
+    {
+        var rewardId = Guid.NewGuid();
+        var visitorId = Guid.NewGuid();
+        var date = new DateTime(2025, 12, 19, 14, 30, 00);
+
+        var redemption = new RewardRedemption { RewardId = rewardId, VisitorId = visitorId, Date = date };
+
+        redemption.Date.Should().Be(date);
+    }
 }

ee970af611ffa159f1914b81b17367733c4668bd
gdianesi <gd329180@fi365.ort.edu.uy>
2025-10-20 16:25:33 -0300

    GREEN: createRewardRequest description

--- tests/VirtualPark.WebApi.Test/Controllers/Reward/ModelsIn/CreateRewardRequestTest.cs
+++ tests/VirtualPark.WebApi.Test/Controllers/Reward/ModelsIn/CreateRewardRequestTest.cs
@@ -8,11 +8,20 @@ namespace VirtualPark.WebApi.Test.Controllers.Reward.ModelsIn;
 [TestCategory("CreateRewardRequest")]
 public class CreateRewardRequestTest
 {
+    #region Name
     [TestMethod]
     [TestCategory("Validation")]
     public void Name_Getter_ReturnsAssignedValue()
     {
         var request = new CreateRewardRequest { Name = "VIP Ticket" };
         request.Name.Should().Be("VIP Ticket");
+    }
+    #endregion
+    [TestMethod]
+    [TestCategory("Validation")]
+    public void Description_Getter_ReturnsAssignedValue()
+    {
+        var request = new CreateRewardRequest { Description = "VIP entry with priority access" };
+        request.Description.Should().Be("VIP entry with priority access");
     }
 }

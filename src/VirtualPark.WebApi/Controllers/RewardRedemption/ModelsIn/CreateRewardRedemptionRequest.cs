b4114089247c708b79271d2b1d16d76c324d0db5
gdianesi <gd329180@fi365.ort.edu.uy>
2025-10-21 20:56:13 -0300

    GREEN: idVisitor in create reward redemption request

--- src/VirtualPark.WebApi/Controllers/RewardRedemption/ModelsIn/CreateRewardRedemptionRequest.cs
+++ src/VirtualPark.WebApi/Controllers/RewardRedemption/ModelsIn/CreateRewardRedemptionRequest.cs
@@ -3,4 +3,5 @@ namespace VirtualPark.WebApi.Controllers.RewardRedemption.ModelsIn;
 public sealed class CreateRewardRedemptionRequest
 {
     public string? RewardId { get; init; }
+    public string? VisitorId { get; init; }
 }

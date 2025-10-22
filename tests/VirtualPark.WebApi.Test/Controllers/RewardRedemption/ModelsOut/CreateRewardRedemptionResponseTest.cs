4073e3e4c2ae5822af366b15477ff93ad5b1bd8a
gdianesi <gd329180@fi365.ort.edu.uy>
2025-10-21 21:18:18 -0300

    REFACTOR: id in create reward redemption response

--- tests/VirtualPark.WebApi.Test/Controllers/RewardRedemption/ModelsOut/CreateRewardRedemptionResponseTest.cs
+++ tests/VirtualPark.WebApi.Test/Controllers/RewardRedemption/ModelsOut/CreateRewardRedemptionResponseTest.cs
@@ -8,6 +8,7 @@ namespace VirtualPark.WebApi.Test.Controllers.RewardRedemption.ModelsOut;
 [TestCategory("CreateRewardRedemptionResponse")]
 public class CreateRewardRedemptionResponseTest
 {
+    #region Id
     [TestMethod]
     [TestCategory("Validation")]
     public void Id_Getter_ReturnsAssignedValue()
@@ -18,4 +19,5 @@ public class CreateRewardRedemptionResponseTest
 
         response.Id.Should().Be(id);
     }
+    #endregion
 }

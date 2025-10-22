32dc63e7ce1a88d94f95b0efc1349de95b7ee9e7
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-21 22:01:57 -0300

    create buttons component and refactor home

--- VirtualParkUI/src/app/app.module.ts
+++ VirtualParkUI/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './app/pages/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ButtonsComponent } from './components/buttons/buttons.component';
 
 @NgModule({
-    declarations: [AppComponent, HomeComponent],
+    declarations: [AppComponent, HomeComponent, ButtonsComponent],
     imports: [BrowserModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent]

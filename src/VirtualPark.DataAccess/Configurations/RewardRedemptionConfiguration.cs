e2db99e8f9b3afefc36a5f979d1bfa37abe170c2
gdianesi <gd329180@fi365.ort.edu.uy>
2025-10-21 22:19:53 -0300

    add migrations

---
+++ src/VirtualPark.DataAccess/Configurations/RewardRedemptionConfiguration.cs
@@ -0,0 +1,40 @@
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VirtualPark.BusinessLogic.RewardRedemptions.Entity;
+
+namespace VirtualPark.DataAccess.Configurations;
+
+[ExcludeFromCodeCoverage]
+public sealed class RewardRedemptionConfiguration : IEntityTypeConfiguration<RewardRedemption>
+{
+    public void Configure(EntityTypeBuilder<RewardRedemption> entity)
+    {
+        entity.ToTable("RewardRedemptions");
+
+        entity.HasKey(r => r.Id);
+        entity.Property(r => r.Id).ValueGeneratedNever();
+
+        entity.Property(r => r.Date)
+            .IsRequired()
+            .HasColumnType("date")
+            .HasConversion(
+                d => d.ToDateTime(TimeOnly.MinValue),
+                dt => DateOnly.FromDateTime(dt));
+
+        entity.Property(r => r.PointsSpent)
+            .IsRequired();
+
+        entity
+            .HasOne<VirtualPark.BusinessLogic.Rewards.Entity.Reward>()
+            .WithMany()
+            .HasForeignKey(r => r.RewardId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        entity
+            .HasOne<VirtualPark.BusinessLogic.VisitorsProfile.Entity.VisitorProfile>()
+            .WithMany()
+            .HasForeignKey(r => r.VisitorId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}

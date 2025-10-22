7297a1646a4c851efcff2931be08dcd1f5154e8a
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-22 14:28:40 -0300

    RED: RecordVisitScoreArgs  id should be getter

---
+++ tests/VirtualPark.BusinessLogic.Test/VisitsScore/Models/RecordVisitScoreArgsTest.cs
@@ -0,0 +1,21 @@
+namespace VirtualPark.BusinessLogic.Test.VisitsScore.Models;
+
+[TestClass]
+[TestCategory("Model")]
+[TestCategory("RecordVisitScoreArgs")]
+public class RecordVisitScoreArgsTest
+{
+    #region VisitorProfileId
+    #region Get
+    [TestMethod]
+    [TestCategory("Getter")]
+    public void VisitorProfileId_Getter_ShouldReturnAssignedInstance()
+    {
+        var id = Guid.NewGuid();
+        var args = new RecordVisitScoreArgs { VisitorProfileId = id };
+
+        args.VisitorProfileId.Should().Be(id);
+    }
+    #endregion
+    #endregion
+}

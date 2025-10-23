64339e278eb389dd1e1820c811c3bf060eaa855c
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-23 19:52:28 -0300

    refactor drop down menu

--- VirtualParkUI/src/app/components/dropdown-menu/dropdown-menu.component.ts
+++ VirtualParkUI/src/app/components/dropdown-menu/dropdown-menu.component.ts
@@ -3,12 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dropdown-menu',
   standalone: false,
-  template: `
-    <p>
-      dropdown-menu works!
-    </p>
-  `,
-  styles: ``
+  templateUrl: './headerComponent.html',
+  styles: ['./headerComponent.css']
 })
 export class DropdownMenuComponent {
 

b9119305e606d35a206020dbebf83ea1a881fae8
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-10-24 14:07:52 -0300

    GREEN: VisitScoreRequest toArgs

--- src/VirtualPark.WebApi/Controllers/VisitsScore/ModelsIn/VisitScoreRequest.cs
+++ src/VirtualPark.WebApi/Controllers/VisitsScore/ModelsIn/VisitScoreRequest.cs
@@ -1,3 +1,6 @@
+using VirtualPark.BusinessLogic.Validations.Services;
+using VirtualPark.BusinessLogic.VisitsScore.Models;
+
 namespace VirtualPark.WebApi.Controllers.VisitsScore.ModelsIn;
 
 public class VisitScoreRequest
@@ -5,4 +8,11 @@ public class VisitScoreRequest
     public string? VisitRegistrationId { get; init; }
     public string? Origin { get; init; }
     public string? Points { get; init; }
+    public RecordVisitScoreArgs ToArgs()
+    {
+        return new RecordVisitScoreArgs(
+            ValidationServices.ValidateNullOrEmpty(VisitRegistrationId),
+            ValidationServices.ValidateNullOrEmpty(Origin),
+            Points);
+    }
 }

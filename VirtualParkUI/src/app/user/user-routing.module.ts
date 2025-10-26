1437776743ba1e4cbec30c9f9870e4934d60f8af
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-26 12:55:34 -0300

    creat regitser user page and refcator buttons components

--- VirtualParkUI/src/app/user/user-routing.module.ts
+++ VirtualParkUI/src/app/user/user-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserRegisterPageComponent } from './user-register-page/user-register-page.component';
 
 const routes: Routes = [
-    { path: '/register', component: UserRegisterPageComponent }
+    { path: 'register', component: UserRegisterPageComponent }
 ];
 
 @NgModule({

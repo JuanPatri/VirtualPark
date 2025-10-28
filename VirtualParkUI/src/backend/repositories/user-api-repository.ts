3053c8d5a0b6d28d6b3eecd0103bd9dbb9a52265
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-27 21:21:14 -0300

    create user model

--- VirtualParkUI/src/backend/repositories/user-api-repository.ts
+++ VirtualParkUI/src/backend/repositories/user-api-repository.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import GenericApiRepository from './generic-api-repository';
+import { CreateUserRequest } from '../services/user/models/CreateUserRequest';
+import { CreateUserResponse } from '../services/user/models/CreateUserResponse';
+import { }
 
 @Injectable({ providedIn: 'root' })
 export class UserApiRepository extends GenericApiRepository {
@@ -9,5 +12,5 @@ export class UserApiRepository extends GenericApiRepository {
         super('users', http);
     }
     
-    
+    public getAllUsers(): Observable<Users>
 }
---
+++ VirtualParkUI/src/backend/services/user/models/UserModel.ts
@@ -0,0 +1,8 @@
+export interface UserModel {
+    id: string;
+    name: string;
+    lastName: string;
+    email: string;
+    roles: RolesModel[];
+    visitorProfile: VisitorProfileModel[];
+}

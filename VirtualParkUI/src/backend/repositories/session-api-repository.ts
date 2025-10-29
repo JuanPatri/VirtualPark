c4a57723f7dd1c88db1b64f531441f1b3c6035ad
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-28 22:17:43 -0300

    refactor SessionApiRepository

--- VirtualParkUI/src/backend/repositories/session-api-repository.ts
+++ VirtualParkUI/src/backend/repositories/session-api-repository.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import GenericApiRepository from './generic-api-repository';
 import { LoginRequest } from '../services/session/models/LoginRequest' 
 import { LoginResponse } from '../services/session/models/LoginResponse';
+import { GetLoginResponse } from '../services/session/models/GetLoginResponse';
 
 @Injectable({
     providedIn: 'root'
@@ -21,7 +22,7 @@ export class SessionApiRepository extends GenericApiRepository {
         return this.deleteById<void>(token);
     }
 
-    getSessionByToken(token: string): Observable<LoginResponse> {
-        return this.getById<LoginResponse>(token);
+    getSessionByToken(token: string): Observable<GetLoginResponse> {
+        return this.getById<GetLoginResponse>(token);
     }
 }

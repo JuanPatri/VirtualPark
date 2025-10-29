259f3320d8176a7cbee7647ca8cef33889c90129
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-10-28 22:44:45 -0300

    refactor user module

--- VirtualParkUI/src/backend/repositories/session-api-repository.ts
+++ VirtualParkUI/src/backend/repositories/session-api-repository.ts
@@ -1,25 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap, finalize } from 'rxjs';
 import GenericApiRepository from './generic-api-repository';
-import { LoginRequest } from '../services/session/models/LoginRequest' 
+import { LoginRequest } from '../services/session/models/LoginRequest';
 import { LoginResponse } from '../services/session/models/LoginResponse';
 import { GetSessionResponse } from '../services/session/models/GetSessionResponse';
 
-@Injectable({
-    providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class SessionApiRepository extends GenericApiRepository {
     constructor(http: HttpClient) {
         super('sessions', http);
     }
 
     login(credentials: LoginRequest): Observable<LoginResponse> {
-        return this.create<LoginResponse>(credentials, false);
+        return this.create<LoginResponse>(credentials, false).pipe(
+            tap(res => {
+                if (res?.token) localStorage.setItem('token', res.token);
+            })
+        );
     }
 
     logout(token: string): Observable<void> {
-        return this.deleteById<void>(token);
+        return this.deleteById<void>(token).pipe(
+            finalize(() => localStorage.removeItem('token'))
+        );
     }
 
     getSessionByToken(token: string): Observable<GetSessionResponse> {

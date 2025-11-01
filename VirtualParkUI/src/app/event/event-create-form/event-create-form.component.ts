8bf2b6e2e713450fcde42b4eed44a4a8cc65c43f
gdianesi <gd329180@fi365.ort.edu.uy>
2025-11-01 15:32:53 -0300

    create event component

---
+++ VirtualParkUI/src/app/event/event-create-form/event-create-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-create-form',
+  standalone: false,
+  templateUrl: './event-create-form.component.html',
+  styleUrl: './event-create-form.component.css'
+})
+export class EventCreateFormComponent {
+
+}
--- VirtualParkUI/src/app/event/event.module.ts
+++ VirtualParkUI/src/app/event/event.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { EventRoutingModule } from './event-routing.module';
 import { EventPageComponent } from './event-list-page/event-page.component';
 import { ButtonsComponent } from '../components/buttons/buttons.component';
+import { EventCreateFormComponent } from './event-create-form/event-create-form.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    EventCreateFormComponent
+  ],
   imports: [
     CommonModule,
     EventRoutingModule,

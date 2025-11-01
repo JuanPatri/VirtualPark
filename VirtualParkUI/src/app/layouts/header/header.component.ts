76566a2c9404095917a2d4bb3f74b9d4ba4909a1
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-11-01 01:14:29 -0300

    Header component

--- VirtualParkUI/src/app/layouts/header/header.component.ts
+++ VirtualParkUI/src/app/layouts/header/header.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { DropdownMenuComponent } from '../../components/dropdown-menu/dropdown-menu.component';
 
 @Component({
     selector: 'app-header',
-    standalone: true,     
-    imports: [DropdownMenuComponent],
+    standalone: true,
+    imports: [CommonModule, RouterModule, DropdownMenuComponent],
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+    attractionsMenu = [
+        { label: 'List', path: '/attraction' },
+        { label: 'Create', path: '/attraction/new' },
+        { label: 'Ranking', path: '/ranking' }
+    ];
+
+    eventsMenu = [
+        { label: 'Event', path: '/events' },
+        { label: 'Create', path: '/events/new' }
+    ];
+
+    rewardMenu = [
+        { label: 'Reward', path: '/reward' },
+        { label: 'Create', path: '/rewards/create' },
+        
+    ]
+}

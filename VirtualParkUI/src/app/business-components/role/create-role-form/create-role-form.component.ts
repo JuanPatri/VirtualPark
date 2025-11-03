0a83e37a8a13e905b66865134639be4d75b1b1a9
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-11-03 20:07:43 -0300

    create role form files

---
+++ VirtualParkUI/src/app/business-components/role/create-role-form/create-role-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-role-form',
+  standalone: false,
+  templateUrl: './create-role-form.component.html',
+  styleUrl: './create-role-form.component.css'
+})
+export class CreateRoleFormComponent {
+
+}

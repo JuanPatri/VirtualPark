f7463d071ced342cbf010a7f01c94c3a51f58473
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-11-05 22:32:30 -0300

    RED: GetImplementations_WhenMixOfStrategyAndNonStrategyDlls_ShouldReturnOnlyStrategies

--- tests/VirtualPark.Reflection.Test/LoadAssemblyTest.cs
+++ tests/VirtualPark.Reflection.Test/LoadAssemblyTest.cs
@@ -79,6 +79,32 @@ public sealed class LoadAssemblyTest
         result.Should().Contain(typeof(EventPointsStrategy).FullName!);
     }
 
+    [TestMethod]
+    public void GetImplementations_WhenMixOfStrategyAndNonStrategyDlls_ShouldReturnOnlyStrategies()
+    {
+        var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "PluingsTestDllTest_Mixed");
+        if (Directory.Exists(path))
+        {
+            Directory.Delete(path, true);
+        }
+
+        Directory.CreateDirectory(path);
+
+        var strategyDll = typeof(AttractionPointsStrategy).Assembly.Location;
+        var strategyDest = Path.Combine(path, "VirtualPark.Strategies.Copy.dll");
+        File.Copy(strategyDll, strategyDest, true);
+
+        var testDll = typeof(LoadAssemblyTest).Assembly.Location;
+        var testDest = Path.Combine(path, "VirtualPark.Tests.Copy.dll");
+        File.Copy(testDll, testDest, true);
+
+        var loader = new LoadAssembly<IStrategy>(path);
+
+        Action act = () => loader.GetImplementations();
+
+        act.Should().Throw<InvalidOperationException>()
+            .WithMessage("No strategies found in assembly 'VirtualPark.Tests.Copy.dll'.");
+    }
 
     #endregion
 }

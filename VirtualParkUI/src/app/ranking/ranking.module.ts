b0e1ffbae45a3b95d456d7070f036c8a83618b20
gdianesi <gd329180@fi365.ort.edu.uy>
2025-11-06 14:35:17 -0300

    add rsanking to header

--- VirtualParkUI/src/app/ranking/ranking.module.ts
+++ VirtualParkUI/src/app/ranking/ranking.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { RankingRoutingModule } from './ranking-routing.module';
-import { RankingListPageComponent } from './ranking-list-page/ranking-list-page.component';
+
 
 
 @NgModule({
-  declarations: [
-    RankingListPageComponent
-  ],
+  declarations: [],
   imports: [
     CommonModule,
     RankingRoutingModule

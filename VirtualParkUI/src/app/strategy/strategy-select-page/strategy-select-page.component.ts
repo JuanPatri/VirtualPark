990c6b505636132b76eec112b5421fd365cac163
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-11-07 19:39:21 -0300

    Create module and component strategy

---
+++ VirtualParkUI/src/app/strategy/strategy-select-page/strategy-select-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-strategy-select-page',
+  standalone: false,
+  templateUrl: './strategy-select-page.component.html',
+  styleUrl: './strategy-select-page.component.css'
+})
+export class StrategySelectPageComponent {
+
+}
---
+++ VirtualParkUI/src/app/strategy/strategy.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StrategySelectPageComponent } from './strategy-select-page/strategy-select-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    StrategySelectPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class StrategyModule { }

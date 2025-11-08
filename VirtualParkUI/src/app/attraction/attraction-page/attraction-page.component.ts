02a478b7670cc9fd5028bd7a9c9c7c13dc6d35a7
gdianesi <gd329180@fi365.ort.edu.uy>
2025-11-08 15:42:27 -0300

    add attraction page (not finished) and delete app.module

---
+++ VirtualParkUI/src/app/attraction/attraction-page/attraction-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attraction-page',
+  standalone: false,
+  templateUrl: './attraction-page.component.html',
+  styleUrl: './attraction-page.component.css'
+})
+export class AttractionPageComponent {
+
+}
--- VirtualParkUI/src/app/attraction/attraction.module.ts
+++ VirtualParkUI/src/app/attraction/attraction.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { AttractionRoutingModule } from './attraction-routing.module';
+import { AttractionPageComponent } from './attraction-page/attraction-page.component';
 
 @NgModule({
   imports: [AttractionRoutingModule],
+  declarations: [
+    AttractionPageComponent
+  ],
 })
 export class AttractionModule {}

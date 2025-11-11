8a603d417a1242665a9d6988f2d9bc484787563f
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-11-11 19:46:07 -0300

    Refactor strategy routing module

--- VirtualParkUI/src/app/strategy/strategy.module.ts
+++ VirtualParkUI/src/app/strategy/strategy.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StrategySelectPageComponent } from './strategy-select-page/strategy-select-page.component';
-import { StrategyRoutingModule } from './strategy-routing';
+import { StrategyRoutingModule } from './strategy-routing.module';
 import { ButtonsComponent } from '../components/buttons/buttons.component';
 import { StrategyPageComponent } from './strategy-page/strategy-page.component';
 

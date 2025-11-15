d7d27fae4dbf7fa41d2dabdaba5f15e53319d3a8
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-11-15 18:20:22 -0300

    RoleRoutingModule

--- VirtualParkUI/src/app/role/role-routing.module.ts
+++ VirtualParkUI/src/app/role/role-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import{ RolePageComponent } from './role-page/role-page.component';
+import { RolePermissionPageComponent } from './role-permission-page/role-permission-page.component';
 
-const routes: Routes = []
+const routes: Routes = [
+    { path: '', component: RolePageComponent,
+      children: [
+        { path: 'permissions', component: RolePermissionPageComponent }
+      ]}
+]
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],

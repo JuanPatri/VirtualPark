d0b1edac83c745e5dd9a514425441c023d8e1761
gdianesi <gd329180@fi365.ort.edu.uy>
2025-11-15 14:01:07 -0300

    remove unused inits

--- src/VirtualPark.BusinessLogic/VisitsScore/Models/RecordVisitScoreArgs.cs
+++ src/VirtualPark.BusinessLogic/VisitsScore/Models/RecordVisitScoreArgs.cs
@@ -4,7 +4,7 @@ namespace VirtualPark.BusinessLogic.VisitsScore.Models;
 
 public sealed class RecordVisitScoreArgs(string visitRegistrationId, string origin, string? points = null)
 {
-    public Guid VisitRegistrationId { get; init; } = ValidationServices.ValidateAndParseGuid(visitRegistrationId);
-    public string Origin { get; init; } = ValidationServices.ValidateNullOrEmpty(origin).Trim();
-    public int? Points { get; init; } = string.IsNullOrWhiteSpace(points) ? null : ValidationServices.ValidateAndParseInt(points);
+    public Guid VisitRegistrationId { get; } = ValidationServices.ValidateAndParseGuid(visitRegistrationId);
+    public string Origin { get; } = ValidationServices.ValidateNullOrEmpty(origin).Trim();
+    public int? Points { get; } = string.IsNullOrWhiteSpace(points) ? null : ValidationServices.ValidateAndParseInt(points);
 }

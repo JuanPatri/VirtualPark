a16c4a5833f31410ca9089f5bad1283e0e466a7a
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-11-16 13:07:19 -0300

    Create role list page

---
+++ VirtualParkUI/src/app/role/role-list-page/role-list-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RoleService } from '../../../backend/services/role/role.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-role-list-page',
+  standalone: true,
+  templateUrl: './role-list-page.component.html',
+  styleUrl: './role-list-page.component.css'
+})
+export class RoleListPageComponent {
+
+}
--- VirtualParkUI/src/app/role/role.module.ts
+++ VirtualParkUI/src/app/role/role.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RoleRoutingModule } from './role-routing.module';
 import { RolePageComponent } from './role-page/role-page.component';
 import { RoleCreatePageComponent } from './role-create-page/role-create-page.component';
+import { RoleListPageComponent } from './role-list-page/role-list-page.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RoleListPageComponent
+  ],
   imports: [CommonModule, RoleRoutingModule, RolePageComponent, RoleCreatePageComponent]
 })
 export class RoleModule { }

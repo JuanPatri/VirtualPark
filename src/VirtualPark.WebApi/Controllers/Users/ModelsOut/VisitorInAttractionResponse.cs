4c8ac1b3478e772d13ab314952b0bcb640dee0f7
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-11-16 17:58:55 -0300

    GREEN: visitorInAttractionResponse userid

--- src/VirtualPark.WebApi/Controllers/Users/ModelsOut/VisitorInAttractionResponse.cs
+++ src/VirtualPark.WebApi/Controllers/Users/ModelsOut/VisitorInAttractionResponse.cs
@@ -2,5 +2,6 @@ namespace VirtualPark.WebApi.Controllers.Users.ModelsOut;
 
 public class VisitorInAttractionResponse
 {
-    public Guid? VisitorProfileId { get; init; }
+    public Guid VisitorProfileId { get; init; }
+    public Guid UserId { get; init; }
 }

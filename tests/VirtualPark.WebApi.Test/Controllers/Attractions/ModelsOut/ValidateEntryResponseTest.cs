a37849656fa4a72312f11d00ff9f667582f49bdb
Alfonso Ricaldoni <AR287195@fi365.ort.edu.uy>
2025-11-16 11:56:38 -0300

    RED: validateEntryResponse is valid

---
+++ tests/VirtualPark.WebApi.Test/Controllers/Attractions/ModelsOut/ValidateEntryResponseTest.cs
@@ -0,0 +1,16 @@
+namespace VirtualPark.WebApi.Test.Controllers.Attractions.ModelsOut;
+
+[TestClass]
+[TestCategory("ValidateEntryResponse")]
+public class ValidateEntryResponseTest
+{
+    #region IsValid
+    [TestMethod]
+    public void ValidateEntryResponse_IsValidProperty_GetAndSet_ShouldWorkCorrectly()
+    {
+        var response = new ValidateEntryResponse { IsValid = true };
+
+        response.IsValid.Should().BeTrue();
+    }
+    #endregion
+}

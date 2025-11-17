da89f3f303d501cc74fb9ecd27d5b0cde07f9c6e
gdianesi <gd329180@fi365.ort.edu.uy>
2025-11-17 20:09:42 -0300

    RED: getDeleted method

---
+++ VirtualParkUI/src/app/attraction/attraction-deleted/attraction-deleted.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attraction-deleted',
+  standalone: false,
+  templateUrl: './attraction-deleted.component.html',
+  styleUrl: './attraction-deleted.component.css'
+})
+export class AttractionDeletedComponent {
+
+}
--- VirtualParkUI/src/app/attraction/attraction.module.ts
+++ VirtualParkUI/src/app/attraction/attraction.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { AttractionRoutingModule } from './attraction-routing.module';
 import { AttractionPageComponent } from './attraction-page/attraction-page.component';
+import { AttractionDeletedComponent } from './attraction-deleted/attraction-deleted.component';
 
 @NgModule({
   imports: [AttractionRoutingModule, AttractionPageComponent],
   declarations: [
+  
+    AttractionDeletedComponent
   ],
 })
 export class AttractionModule {}

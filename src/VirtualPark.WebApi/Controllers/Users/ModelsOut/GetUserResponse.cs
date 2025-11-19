e9e4f2d5d05409bfc84d0c9e6055cbeec5d41e77
gdianesi <gd329180@fi365.ort.edu.uy>
2025-11-19 15:00:28 -0300

    same wtih users

--- src/VirtualPark.WebApi/Controllers/Users/ModelsOut/GetUserResponse.cs
+++ src/VirtualPark.WebApi/Controllers/Users/ModelsOut/GetUserResponse.cs
@@ -1,11 +1,26 @@
+using VirtualPark.BusinessLogic.Users.Entity;
+
 namespace VirtualPark.WebApi.Controllers.Users.ModelsOut;
 
-public class GetUserResponse(string id, string name, string lastName, string email, List<string> roles, string? visitorProfileId)
+public class GetUserResponse
 {
-    public string Id { get; } = id;
-    public string Name { get; } = name;
-    public string LastName { get; } = lastName;
-    public string Email { get; } = email;
-    public List<string> Roles { get; } = roles;
-    public string? VisitorProfileId { get; } = visitorProfileId;
+    public string Id { get; }
+    public string Name { get; }
+    public string LastName { get; }
+    public string Email { get; }
+    public List<string> Roles { get; }
+    public string? VisitorProfileId { get; }
+
+    public GetUserResponse(User user)
+    {
+        Id = user.Id.ToString();
+        Name = user.Name;
+        LastName = user.LastName;
+        Email = user.Email;
+        Roles = user.Roles?
+            .Select(r => r.Id.ToString())
+            .ToList() ?? [];
+
+        VisitorProfileId = user.VisitorProfileId?.ToString();
+    }
 }

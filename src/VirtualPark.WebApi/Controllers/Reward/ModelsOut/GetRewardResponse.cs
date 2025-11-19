03eb03af360c739cc11b9811810a07a57bc76d5e
gdianesi <gd329180@fi365.ort.edu.uy>
2025-11-19 14:42:25 -0300

    same with rewards

--- src/VirtualPark.WebApi/Controllers/Reward/ModelsOut/GetRewardResponse.cs
+++ src/VirtualPark.WebApi/Controllers/Reward/ModelsOut/GetRewardResponse.cs
@@ -1,11 +1,21 @@
 namespace VirtualPark.WebApi.Controllers.Reward.ModelsOut;
 
-public class GetRewardResponse(string id, string name, string description, string cost, string quantity, string membership)
+public class GetRewardResponse
 {
-    public string Id { get; } = id;
-    public string Name { get; } = name;
-    public string Description { get; } = description;
-    public string Cost { get; } = cost;
-    public string QuantityAvailable { get; } = quantity;
-    public string Membership { get; } = membership;
+    public string Id { get; }
+    public string Name { get; }
+    public string Description { get; }
+    public string Cost { get; }
+    public string QuantityAvailable { get; }
+    public string Membership { get; }
+
+    public GetRewardResponse(BusinessLogic.Rewards.Entity.Reward reward)
+    {
+        Id = reward.Id.ToString();
+        Name = reward.Name;
+        Description = reward.Description;
+        Cost = reward.Cost.ToString();
+        QuantityAvailable = reward.QuantityAvailable.ToString();
+        Membership = reward.RequiredMembershipLevel.ToString();
+    }
 }

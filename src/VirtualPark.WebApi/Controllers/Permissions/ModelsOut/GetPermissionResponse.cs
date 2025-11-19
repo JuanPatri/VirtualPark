e88acdca0098bf07fead4f773aa7fafb874c63fa
gdianesi <gd329180@fi365.ort.edu.uy>
2025-11-19 14:40:15 -0300

    same with ranking

--- src/VirtualPark.WebApi/Controllers/Permissions/ModelsOut/GetPermissionResponse.cs
+++ src/VirtualPark.WebApi/Controllers/Permissions/ModelsOut/GetPermissionResponse.cs
@@ -1,9 +1,21 @@
+using VirtualPark.BusinessLogic.Permissions.Entity;
+
 namespace VirtualPark.WebApi.Controllers.Permissions.ModelsOut;
 
-public class GetPermissionResponse(string id, string description, string key, List<string> roles)
+public class GetPermissionResponse
 {
-    public string Id { get; } = id;
-    public string Description { get; } = description;
-    public string Key { get; } = key;
-    public List<string> Roles { get; } = roles;
+    public string Id { get; }
+    public string Description { get; }
+    public string Key { get; }
+    public List<string> Roles { get; }
+
+    public GetPermissionResponse(Permission permission)
+    {
+        Id = permission.Id.ToString();
+        Description = permission.Description;
+        Key = permission.Key;
+        Roles = permission.Roles
+            .Select(r => r.Id.ToString())
+            .ToList();
+    }
 }
--- src/VirtualPark.WebApi/Controllers/Ranking/ModelsOut/GetRankingResponse.cs
+++ src/VirtualPark.WebApi/Controllers/Ranking/ModelsOut/GetRankingResponse.cs
@@ -1,10 +1,26 @@
 namespace VirtualPark.WebApi.Controllers.Ranking.ModelsOut;
 
-public sealed class GetRankingResponse(string id, string date, List<string> users, List<string> scores, string period)
+public sealed class GetRankingResponse
 {
-    public string Id { get; } = id;
-    public string Date { get; } = date;
-    public List<string> Users { get; } = users;
-    public List<string> Scores { get; } = scores;
-    public string Period { get; } = period;
+    public string Id { get; }
+    public string Date { get; }
+    public List<string> Users { get; }
+    public List<string> Scores { get; }
+    public string Period { get; }
+
+    public GetRankingResponse(BusinessLogic.Rankings.Entity.Ranking? ranking)
+    {
+        Id = ranking.Id.ToString();
+        Date = ranking.Date.ToString("yyyy-MM-dd");
+
+        Users = ranking.Entries
+            .Select(e => e.Id.ToString())
+            .ToList();
+
+        Scores = ranking.Entries
+            .Select(e => (e.VisitorProfile?.Score ?? 0).ToString())
+            .ToList();
+
+        Period = ranking.Period.ToString();
+    }
 }

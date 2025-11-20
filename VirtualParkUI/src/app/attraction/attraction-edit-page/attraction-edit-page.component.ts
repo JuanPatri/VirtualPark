593b3e208c3a13dd695ff582c9d8f617a71b7ef2
gdianesi <gd329180@fi365.ort.edu.uy>
2025-11-20 13:46:53 -0300

    fix messages format

--- VirtualParkUI/src/app/attraction/attraction-edit-page/attraction-edit-page.component.ts
+++ VirtualParkUI/src/app/attraction/attraction-edit-page/attraction-edit-page.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { ButtonsComponent } from '../../components/buttons/buttons.component';
 import { AttractionFormValue, AttractionInitial, CreateAttractionFormComponent } from '../../business-components/attraction/create-attraction-form/create-attraction-form.component';
 import { AttractionService } from '../../../backend/services/attraction/attraction.service';
 import { GetAttractionResponse } from '../../../backend/services/attraction/models/GetAttractionRequest';
+import { MessageService } from '../../../backend/services/message/message.service';
+import { MessageComponent } from '../../components/messages/message.component';
 
 @Component({
   selector: 'app-attraction-edit-page',
   standalone: true,
-  imports: [RouterLink, ButtonsComponent, CreateAttractionFormComponent],
+  imports: [RouterLink, ButtonsComponent, CreateAttractionFormComponent, MessageComponent],
   templateUrl: './attraction-edit-page.component.html',
   styleUrls: ['./attraction-edit-page.component.css']
 })
 export class AttractionEditPageComponent implements OnInit {
   private route = inject(ActivatedRoute);
   private attractionService = inject(AttractionService);
+  private messageService = inject(MessageService);
+  private router = inject(Router);
 
   id = this.route.snapshot.paramMap.get('id')!;
   initial: AttractionInitial | undefined;
@@ -22,16 +26,20 @@ export class AttractionEditPageComponent implements OnInit {
   ngOnInit(): void {
     this.attractionService.getById(this.id).subscribe({
       next: (dto) => this.initial = this.mapInitial(dto),
-      error: () => alert('No se pudo cargar la atracción')
+      error: (e) => this.messageService.show(e.error?.message ?? 'Error creating attraction.', 'error')
     });
   }
 
-  onSubmit(payload: AttractionFormValue) {
-    this.attractionService.update(this.id, payload).subscribe({
-      next: () => alert('Attraction actualizada'),
-      error: () => alert('Error actualizando')
-    });
-  }
+onSubmit(payload: AttractionFormValue) {
+  this.attractionService.update(this.id, payload).subscribe({
+    next: () => {
+      this.messageService.show('Attraction updated successfully!', 'success');
+      this.router.navigate(['/attraction']);
+    },
+    error: (e) =>
+      this.messageService.show(e.error?.message ?? 'Error updating attraction.', 'error')
+  });
+}
 
   private mapInitial(dto: GetAttractionResponse): AttractionInitial {
     return {

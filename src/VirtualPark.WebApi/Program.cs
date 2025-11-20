ddbc3218d4c768da8c9a34027fcdc457c9e20558
Juan Pablo Patri Mendoza <juanpatri8@gmail.com>
2025-11-20 20:17:11 -0300

    FIX: Production CORS

--- src/VirtualPark.WebApi/Program.cs
+++ src/VirtualPark.WebApi/Program.cs
@@ -13,34 +13,14 @@ builder.Services.AddControllers(options =>
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAngularClient",
-        policy =>
-        {
-            if(builder.Environment.IsDevelopment())
-            {
-                policy.SetIsOriginAllowed(origin =>
-                    {
-                        if(string.IsNullOrWhiteSpace(origin))
-                        {
-                            return false;
-                        }
-
-                        var uri = new Uri(origin);
-                        return uri.Host == "localhost" || uri.Host == "127.0.0.1";
-                    })
-                    .AllowAnyHeader()
-                    .AllowAnyMethod()
-                    .AllowCredentials();
-            }
-            else
-            {
-                policy.WithOrigins(
-                        "https://prod.com")
-                    .AllowAnyHeader()
-                    .AllowAnyMethod()
-                    .AllowCredentials();
-            }
-        });
+    options.AddPolicy("AllowAngularClient", policy =>
+    {
+        policy
+            .SetIsOriginAllowed(_ => true)
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
 });
 
 ServiceFactory.RegisterServices(builder.Services, builder.Configuration);
